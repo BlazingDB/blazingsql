6b360f6967ba7fe5e6f8b2c4e7d4864e3523a99d
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-10-24 02:29:04 -0500

    Avoid unnecessary dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,13 @@
-import os
-import shutil
-import subprocess
-import sys
-import tempfile
-
 from setuptools import setup, find_packages
 
 setup(
-  name='PyBlazing',
-  version='0.1',
-  author='BlazingDB Team',
-  packages=find_packages(),
-  install_requires=(
-    'numpy==1.15.2',
-    'pycuda==2018.1.1',
-    'pygdf@git+https://github.com/rapidsai/pygdf.git#egg=pygdf-master',
-  ),
-  zip_safe=False,
+    name='PyBlazing',
+    version='0.1',
+    author='BlazingDB',
+    packages=find_packages(),
+    install_requires=(
+        'numba==0.40.1',  # already depends on numpy-1.15.3
+        'pycuda==2018.1.1'
+    ),
+    zip_safe=False,
 )
