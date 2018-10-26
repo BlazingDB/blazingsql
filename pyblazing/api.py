0bdc17f144ce03def6352576a9f82155f81d5e7b
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-10-26 17:19:03 -0500

    Clean pyblazing api and and create demo scripts

--- pyblazing/api.py
+++ pyblazing/api.py
@@ -413,31 +413,3 @@ def _private_run_query(sql, tables):
     client.close_connection()
 
     return resultSet
-
-
-import pyarrow as pa
-
-
-#arr = pa.RecordBatchStreamReader('/gpu.arrow').read_all()
-#print(arr)
-#df = arr.to_pandas()
-#df = df[['swings', 'tractions']]
-#gdf = gd.DataFrame.from_pandas(df)
-#gdf._cols["swings"]._column.data.mem.get_ipc_handle()._ipc_handle.handle
-# print(gdf.columns)
-# print(gdf._cols["swings"]._column.dtype)
-# gdf._cols["swings"]._column.cffi_view.size
-
-gdf = gen_data_frame(20, 'swings', np.float32)
-
-
-table = 'holas'
-tables = {table: gdf}
-
-#client = _get_client()
-#_reset_table(client, table, gdf)
-
-#result = run_query('select swings, tractions from main.holas', tables)
-result = run_query('select swings from main.holas', tables)
-
-print("hi")
