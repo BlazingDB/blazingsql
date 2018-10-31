92d2023ba54c1b7a1138c1c5be37a64c9afe6113
William Malpica <william@blazingdb.com>
2018-10-31 10:24:49 -0500

    added new data file and modified test

--- examples/old/pyBlazingTest.py
+++ examples/old/pyBlazingTest.py
@@ -5,19 +5,32 @@ import pyblazing
 def main():
   
   nation_tableName = "nation"
-#   nation_columnNames = ["n_nationkey", "n_name", "n_regionkey", "n_comments"]
-  nation_columnNames = ["acol", "bcol", "ccol", "dcol"]
+  nation_columnNames = ["n_nationkey", "n_name", "n_regionkey", "n_comments"]
+#   nation_columnNames = ["acol", "bcol", "ccol", "dcol"]
   nation_columnTypes = ["GDF_INT32", "GDF_INT64", "GDF_INT8", "GDF_INT64"]  # libgdf style types
-
-  filepath = "data/nation.psv"
+  nation_filepath = "data/nation.psv"
   nation_columnTypes = ["int32", "int64", "int", "int64"]  # pygdf/pandas style types
-  gdf = pygdf.read_csv(filepath, delimiter='|', dtype=nation_columnTypes, names=nation_columnNames)
-  print(gdf)
+  nation_gdf = pygdf.read_csv(nation_filepath, delimiter='|', dtype=nation_columnTypes, names=nation_columnNames)
+  print(nation_gdf)
+  
+  region_tableName = "region"
+  region_columnNames = ["r_regionkey", "r_name", "r_comments"]
+  region_columnTypes = ["GDF_INT8", "GDF_INT64", "GDF_INT64"]  # libgdf style types
+  region_filepath = "data/region.psv"
+  region_columnTypes = ["int", "int64", "int64"]  # pygdf/pandas style types
+  region_gdf = pygdf.read_csv(region_filepath, delimiter='|', dtype=region_columnTypes, names=region_columnNames)
+  print(region_gdf)
 
-  tables = {nation_tableName: gdf}
-  result = pyblazing.run_query('select acol + 1, ccol + 2 from main.nation', tables)
-#   result = pyblazing.run_query('select n_nationkey, n_regionkey, n_name from main.nation', tables)
-#   result = pyblazing.run_query('select n_nationkey, n_regionkey from main.nation where n_nationkey > 5', tables)
+  tables = {nation_tableName: nation_gdf, region_tableName: region_gdf}
+  result = pyblazing.run_query('SELECT n.n_nationkey, n.n_regionkey, r.r_regionkey FROM main.nation AS n INNER JOIN main.region AS r ON n.n_regionkey = r.r_regionkey;', tables)
+  
+#   tables = {region_tableName: region_gdf}  
+#   result = pyblazing.run_query('select r_regionkey from main.region', tables)
+  
+#   tables = {nation_tableName: nation_gdf}  
+#   result = pyblazing.run_query('select n_nationkey + 1, n_regionkey + 2 from main.nation', tables)
+#   result = pyblazing.run_query('select acol, ccol, acol + ccol as summy from main.nation', tables)
+#   result = pyblazing.run_query('select acol, ccol from main.nation', tables)
   print(result)
 
   
