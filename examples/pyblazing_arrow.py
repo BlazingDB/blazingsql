ab66e2a05cf618d8f38bfb09e2b9653b25557f70
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-11-01 05:02:45 -0500

    Expose only rapids and pandas api

--- examples/pyblazing_arrow.py
+++ examples/pyblazing_arrow.py
@@ -2,16 +2,13 @@ import pyarrow as pa
 import pyblazing
 
 arrow_table = pa.RecordBatchStreamReader('data/gpu.arrow').read_all()
+df = arrow_table.to_pandas()
+df = df[['swings', 'tractions']]
+print(df)
 
-columns = ('swings', 'tractions')
-filter_cols = [c.name for c in arrow_table.columns if not c.name in columns]
-arrow_table = arrow_table.drop(filter_cols)
-
-print(arrow_table)
-
-tables = {'gpu_info': arrow_table}
-sql = 'select * from main.gpu_info'
-result_gdf = pyblazing.run_query_arrow(sql, tables)
+tables = {'gpu_info': df}
+sql = 'select swings+1, tractions+10 from main.gpu_info'
+result_gdf = pyblazing.run_query_pandas(sql, tables)
 
 print(sql)
 print(result_gdf)
