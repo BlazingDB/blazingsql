0026b122bd3e2ec93f45e283fb70f166d0dc7965
William Malpica <william@blazingdb.com>
2018-11-20 19:18:10 -0600

    fixed pyBlazing so that it works with the new cudf pygdf

--- examples/old/pyBlazingTest.py
+++ examples/old/pyBlazingTest.py
@@ -1,4 +1,4 @@
-import pygdf
+import cudf
 import pyblazing
 
 
@@ -9,20 +9,20 @@ def main():
 #   nation_columnNames = ["acol", "bcol", "ccol", "dcol"]
   nation_columnTypes = ["GDF_INT32", "GDF_INT64", "GDF_INT8", "GDF_INT64"]  # libgdf style types
   nation_filepath = "data/nation.psv"
-  nation_columnTypes = ["int32", "int64", "int", "int64"]  # pygdf/pandas style types
-  nation_gdf = pygdf.read_csv(nation_filepath, delimiter='|', dtype=nation_columnTypes, names=nation_columnNames)
+  nation_columnTypes = ["int32", "int64", "int", "int64"]  # cudf/pandas style types
+  nation_gdf = cudf.read_csv(nation_filepath, delimiter='|', dtype=nation_columnTypes, names=nation_columnNames)
   print(nation_gdf)
   
   region_tableName = "region"
   region_columnNames = ["r_regionkey", "r_name", "r_comments"]
   region_columnTypes = ["GDF_INT8", "GDF_INT64", "GDF_INT64"]  # libgdf style types
   region_filepath = "data/region.psv"
-  region_columnTypes = ["int", "int64", "int64"]  # pygdf/pandas style types
-  region_gdf = pygdf.read_csv(region_filepath, delimiter='|', dtype=region_columnTypes, names=region_columnNames)
+  region_columnTypes = ["int", "int64", "int64"]  # cudf/pandas style types
+  region_gdf = cudf.read_csv(region_filepath, delimiter='|', dtype=region_columnTypes, names=region_columnNames)
   print(region_gdf)
 
   tables = {nation_tableName: nation_gdf, region_tableName: region_gdf}
-  result = pyblazing.run_query('SELECT n.n_nationkey, n.n_regionkey, r.r_regionkey FROM main.nation AS n INNER JOIN main.region AS r ON n.n_regionkey = r.r_regionkey;', tables)
+  result = pyblazing.run_query('SELECT n.n_nationkey, n.n_regionkey, r.r_regionkey FROM main.nation AS n INNER JOIN main.region AS r ON n.n_regionkey = r.r_regionkey', tables)
   
 #   tables = {region_tableName: region_gdf}  
 #   result = pyblazing.run_query('select r_regionkey from main.region', tables)
@@ -31,7 +31,7 @@ def main():
 #   result = pyblazing.run_query('select n_nationkey + 1, n_regionkey + 2 from main.nation', tables)
 #   result = pyblazing.run_query('select acol, ccol, acol + ccol as summy from main.nation', tables)
 #   result = pyblazing.run_query('select acol, ccol from main.nation', tables)
-  print(result)
+  print(result.columns)
 
   
   
