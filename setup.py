6dc80b294e7c538eb9be96ffa977963c57b65477
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2018-11-26 04:53:25 -0500

    Assume cudf is already in the conda env so we don't need this deps

--- setup.py
+++ setup.py
@@ -6,8 +6,6 @@ setup(
     author='BlazingDB',
     packages=find_packages(),
     install_requires=(
-        'numba==0.40.1',  # already depends on numpy-1.15.3
-        'pycuda==2018.1.1'
     ),
     zip_safe=False,
 )
