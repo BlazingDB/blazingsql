565203bb81f23b05e07ad72ad5feff62af1aed6b
Alexander <aocsa.cs@gmail.com>
2018-12-18 11:04:23 -0500

    [blazingdb-io] update example to run queries on parquet files

--- examples/pyblazing_filesystem.py
+++ examples/pyblazing_filesystem.py
@@ -1,14 +1,17 @@
-import pyarrow as pa
+import numpy as np
+import pandas as pd
+import cudf as cudf
 import pyblazing
 from pyblazing import DriverType, FileSystemType, EncryptionType
 from pyblazing import SchemaFrom
 
+
 def register_hdfs():
     print('*** Register a HDFS File System ***')
     fs_status = pyblazing.register_file_system(
-        authority="tpch2",
+        authority="tpch_hdfs",
         type=FileSystemType.HDFS,
-        root="/tmp/tpch/1mb/",
+        root="/",
         params={
             "host": "192.168.0.1",
             "port": 8080,
@@ -23,9 +26,9 @@ def register_hdfs():
 def register_s3():
     print('*** Register a S3 File System ***')
     fs_status = pyblazing.register_file_system(
-        authority="tpch2",
+        authority="tpch_s3",
         type=FileSystemType.S3,
-        root="/tmp/tpch/1mb/",
+        root="/",
         params={
             "fs_name": "company",
             "bucket_name": "company_bucket",
@@ -38,60 +41,13 @@ def register_s3():
     )
     print(fs_status)
 
-import numpy as np
-import pandas as pd
-import cudf as cudf
 
-
-# def load_from_pandas():
-#     column_names = ["n_nationkey", "n_name", "n_regionkey", "n_comments"]
-#     column_types = ["int32", "int64", "int32", "int64"]
-#     nation_df = pd.read_csv("data/nation.psv", delimiter='|', names=column_names)
-#     nation_df = nation_df[['n_nationkey', 'n_regionkey']]
-#     nation_gdf = cudf.DataFrame.from_pandas(nation_df)
-#
-#     nation_schema = Schema(type=SchemaFrom.Gdf, gdf=nation_gdf)
-#     gdf_nation_table = pyblazing.register_table('nation', nation_schema)
-#
-
-def load_and_query_csv_files():
+def main():
     print('*** Register a POSIX File System ***')
     fs_status = pyblazing.register_file_system(
         authority="tpch",
         type=FileSystemType.POSIX,
-        root="/"  # como se usa en ral
-    )
-    print(fs_status)
-    names=['n_nationkey', 'n_name', 'n_regionkey', 'n_comment']
-    dtypes = [3, 4, 3, 4]
-    #todo, estos kwargs deberian coincidir con flatbuffers CsvFile
-        # path: string;
-        # delimiter: string;
-        # lineTerminator: string;
-        # skipRows: int;
-        # names: [string];
-        # dtypes: [int];
-    nation_schema = pyblazing.register_table_schema(table_name='nation_csv', type=SchemaFrom.CsvFile, path='/tmp/tpch/1mb/nation.psv', delimiter='|', dtypes=dtypes, names=names)
-    # query using multiples files
-    sql_data = {
-        nation_schema: ['/tmp/tpch/1mb/nation.psv']
-    }
-    sql = 'select n_nationkey, n_regionkey + n_nationkey as addition from main.nation_csv'
-    result_gdf = pyblazing.run_query_filesystem(sql, sql_data)
-    print(sql)
-    print(result_gdf)
-
-    fs_status = pyblazing.deregister_file_system(authority="tpch")
-    print(fs_status)
-    fs_status = pyblazing.deregister_file_system(authority="tpch")
-    print(fs_status)
-
-def load_and_query_parquet_files():
-    print('*** Register a POSIX File System ***')
-    fs_status = pyblazing.register_file_system(
-        authority="tpch",
-        type=FileSystemType.POSIX,
-        root="/" #como se usa en ral
+        root="/"
     )
     print(fs_status)
 
@@ -112,5 +68,4 @@ def load_and_query_parquet_files():
     print(fs_status)
 
 if __name__ == '__main__':
-    # load_and_query_parquet_files()
-    load_and_query_csv_files()
+    main()
