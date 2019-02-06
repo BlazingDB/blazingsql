daf48d94e2dc0544b47a2e543cec37d015147451
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-02-06 18:45:36 -0500

    1st draft for the new API concepts

---
+++ pyblazing/components.py
@@ -0,0 +1,234 @@
+from collections import OrderedDict
+from enum import Enum
+
+
+class S3(Enum):
+    NONE = 'NONE'
+    AES_256 = 'AES_256'
+    AWS_KMS = 'AWS_KMS'
+
+
+class DataSource:
+
+    def __init__(self):
+        pass
+
+    def is_valid(self):
+        pass
+
+    # cudf.DataFrame in-gpu-memory
+    def is_cudf(self):
+        pass
+
+    # pandas.DataFrame in-memory
+    def is_pandas(self):
+        pass
+
+    # arrow file on filesystem or arrow in-memory
+    def is_arrow(self):
+        pass
+
+    # csv file on filesystem
+    def is_csv(self):
+        pass
+
+    # parquet file on filesystem
+    def is_parquet(self):
+        pass
+
+    # dir which contains any supported file on file system (csv, parquet or arrow)
+    def is_dir(self):
+        pass
+
+# BEGIN DataSource builders
+
+
+def from_cudf():
+    pass
+
+
+def from_pandas():
+    pass
+
+
+def from_arrow():
+    pass
+
+
+# path is file system transparent
+def from_csv(path):
+    pass
+
+
+# path is file system transparent
+def from_parquet(path):
+    pass
+
+
+# path is file system transparent
+def from_dir(path, hint = 'auto can be parquet arrow csv'):
+    pass
+
+# END DataSource builders
+
+
+# Maintains the resulset and the token after the run_query
+class ResultSet:
+
+    # this will call the get_result api
+    def get(self):
+        pass
+
+    # TODO see Rodriugo proposal for interesting actions/operations here
+
+
+class SQL:
+
+    def __init__(self):
+        pass
+
+    # ds is the DataSource object
+    def table(self, name, ds):
+        pass
+
+    # TODO percy this is to save materialized tables avoid reading from the data source
+    def view(self, name, sql):
+        pass
+
+    # TODO percy drops should be here but this will be later (see Felipe proposal free)
+    def drop(self, name):
+        pass
+
+    # TODO percy think about William proposal, launch, token split and distribution use case
+    # return result obj ... by default is async
+    def run_query(self, sql, async_opts = 'TODO'):
+        rs = ResultSet()
+        # TODO percy
+        return rs
+
+
+class FileSystem:
+
+    def __init__(self):
+        self.file_systems = []
+
+    def __repr__(self):
+        return "TODO"
+
+    def __str__(self):
+        pass
+
+    def hdfs(self, prefix, **kwargs):
+        # TODO percy check prefix is unique
+
+        host = kwargs.get('host', '127.0.0.1')
+        port = kwargs.get('port', 8020)
+        user = kwargs.get('user', '')
+        kerberos_ticket = kwargs.get('kerberos_ticket', '')
+
+        fs = OrderedDict()
+        fs['type'] = 'hdfs'
+        fs['prefix'] = prefix
+        fs['host'] = host
+        fs['port'] = port
+        fs['user'] = user
+        fs['kerberos_ticket'] = kerberos_ticket
+
+        self.file_systems.append(fs)
+
+        # TODO percy connect here with low level api
+
+        return fs
+
+    def s3(self, prefix, **kwargs):
+        # TODO percy check prefix is unique
+
+        bucket_name = kwargs.get('bucket_name', '')
+        access_key_id = kwargs.get('access_key_id', '')
+        secret_key = kwargs.get('secret_key', '')
+        session_token = kwargs.get('session_token', '')
+        encryption_type = kwargs.get('encryption_type', S3.NONE)
+        kms_key_amazon_resource_name = kwargs.get('kms_key_amazon_resource_name', '')
+
+        fs = OrderedDict()
+        fs['type'] = 's3'
+        fs['prefix'] = prefix
+        fs['bucket_name'] = bucket_name
+        fs['access_key_id'] = access_key_id
+        fs['secret_key'] = secret_key
+        fs['session_token'] = session_token
+        fs['encryption_type'] = encryption_type
+        fs['kms_key_amazon_resource_name'] = kms_key_amazon_resource_name
+
+        self.file_systems.append(fs)
+
+        # TODO percy connect here with low level api
+
+        return fs
+
+    def show(self):
+        for fs in self.file_systems:
+            type = fs['type']
+            prefix = fs['prefix']
+            fs_str = '%s (%s)' % (prefix, type)
+            print(fs_str)
+
+
+class BlazingContext:
+
+    # connection (string) can be the unix socket path or the tcp host:port
+    def __init__(self, connection):
+        self.connection = connection
+
+        # TODO percy fs and sql can be python modules instead of classes
+        self.fs = FileSystem()
+        self.sql = SQL()
+
+    def __repr__(self):
+        return "BlazingContext('%s')" % (self.connection)
+
+    def __str__(self):
+        return self.connection
+
+
+def make_context(connection):
+    bc = BlazingContext(connection)
+    return bc
+
+###  API usage ###
+
+
+# blazingdb context
+bc = make_context('/tmp/orchestrator.socket')
+print(bc)
+
+# file system
+bc.fs.hdfs('my_fs4', host = '127.0.0.1', port = 54310)
+bc.fs.s3('datastore', bucket_name = 'public_demo', access_key_id = 'ASDD', secret_key = 'SDFG')
+bc.fs.hdfs('prod_fs', host = '127.0.0.1', port = 54310)
+bc.fs.show()  # print all the file systems
+
+# create table using blazing-io
+bc.sql.table('orders', from_csv('s3://my_fs4/home/dat.csv'))
+bc.sql.table('customer', from_parquet('/home/percy/part1.parquet'))
+bc.sql.table('lineitem', from_dir('hdfs://my_fs4/foo/tpch50mb-parquet/'))
+bc.sql.table('bigtable', from_dir('hdfs://prod_fs/percy/tpch3gb-csv/'))
+bc.sql.table('miserablesfb', from_dir('s3://prod_fs/graphistry/arrow-files/gpu/'))
+
+# other table operations (proposals/TODO)
+bc.sql.view('filtered_orders', 'select * from orders where blah')
+bc.sql.drop('bigtable')
+
+# run query (async by default)
+result1 = bc.sql.run_query('select * from orders where foo')
+result2 = bc.sql.run_query('select * from lineitem, miserablesfb where blah')
+
+# resultset operations (get() will call get result under the hood)
+colums1 = result1.get()
+colums2 = result2.get()
+
+# interesting ideas on result objects
+# result.collect.foreach(println) # where println is something like lambda x: print(x)
+# result.show() => implicit call on .get
+
+# TODO register mechanism for distributed tables (IN PROGRESS dask, distribution research)
