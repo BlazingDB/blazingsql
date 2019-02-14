0b7b84cc81e662118583f1de622e5c85e5410a5c
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-02-14 13:56:27 -0500

    William's feedback/review

--- pyblazing/api/example.py
+++ pyblazing/api/example.py
@@ -2,6 +2,7 @@ from components import make_context
 from components import from_csv
 from components import from_parquet
 from components import from_dir
+from examples.blazingsql import gdf_column_type_to_str
 
 ###  API usage ###
 
@@ -15,20 +16,27 @@ bc.fs.s3('datastore', bucket_name = 'public_demo', access_key_id = 'ASDD', secre
 bc.fs.hdfs('prod_fs', host = '127.0.0.1', port = 54310)
 bc.fs.show()  # print all the file systems
 
-# create table using blazing-io
+# create table using blazing-io: from_x seems ok
 bc.sql.table('orders', from_csv('s3://my_fs4/home/dat.csv'))
 bc.sql.table('customer', from_parquet('/home/percy/part1.parquet'))
 bc.sql.table('lineitem', from_dir('hdfs://my_fs4/foo/tpch50mb-parquet/'))
 bc.sql.table('bigtable', from_dir('hdfs://prod_fs/percy/tpch3gb-csv/'))
 bc.sql.table('miserablesfb', from_dir('s3://prod_fs/graphistry/arrow-files/gpu/'))
 
-# other table operations (proposals/TODO)
-bc.sql.view('filtered_orders', 'select * from orders where blah')
-bc.sql.drop('bigtable')
+
+#from felipe ideas
+bc.sql.drop('miserablesfb') # forzar gpu free, blazingsql ownership ... if is last ref the delete
+
+#from william
+#bc.sql.view('miserablesfb', sql, ["oder"])
+
+#on-memory datasources: sin from_
+bc.sql.table('customers', from_gdf(gdf))
+bc.sql.table('customers', from_arrow(arrow))
 
 # run query (async by default)
-result1 = bc.sql.run_query('select * from orders where foo')
-result2 = bc.sql.run_query('select * from lineitem, miserablesfb where blah')
+result1 = bc.sql.run_query('select * from orders where foo', ["orders"])
+result2 = bc.sql.run_query('select * from lineitem, miserablesfb where blah', ["lineitem"])
 
 # resultset operations (get() will call get result under the hood)
 colums1 = result1.get()
