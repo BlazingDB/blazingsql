6d82e30084f18024e0cefd9457d3994271f3eb19
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-02-20 15:28:43 -0500

    More implementation for the sql module and its test

--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,6 +1,7 @@
 import unittest
 
 from pyblazing.apiv2 import sql
+from pyblazing.apiv2 import datasource
 
 from cudf import DataFrame
 
@@ -15,13 +16,20 @@ class TestCreateTableFromGDF(unittest.TestCase):
         cudf_df['key'] = [1, 2, 3, 4, 5]
         cudf_df['val'] = [float(i + 10) for i in range(5)]
 
-        table = self.sql.create_table('holas', cudf_df)
+        ds = datasource.from_cudf(cudf_df)
+
+        table = self.sql.create_table('holas', ds)
 
         print("PRINTTTTTT RAWWWWWWW")
         print(cudf_df)
 
         print("PRINTTTTTT TABLE DATASOURCES")
         print(table)
+        
+        print("RUN QUERYYYYYYYYY")
+        
+        self.run_query()
+
 
 
 if __name__ == '__main__':
