d3a6d0b7fcd0fd0dbca086de55eb7e00dd901bab
jeanp413 <jeanp413@hotmail.com>
2019-07-03 12:40:52 -0500

    Store datasource table inside context. Reverted from 3acf655dc46e9e3b2fa296b19b2e9e8c318f423f

--- pyblazing/apiv2/context.py
+++ pyblazing/apiv2/context.py
@@ -108,6 +108,8 @@ class BlazingContext(object):
 
             # TODO percy dir
 
+        self.sqlObject.create_table(table_name, datasource)
+        
         # TODO percy raise exption here or manage the error
 
         return None
