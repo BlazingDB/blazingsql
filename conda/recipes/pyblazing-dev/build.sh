ee5793e4edc39411ba511c5ff69e17140b4b854f
felipe <felipe@blazingdb.com>
2019-08-15 10:15:57 -0500

    adding conda

---
+++ conda/recipes/pyblazing-dev/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cd $CONDA_PREFIX
+
+git clone https://github.com/BlazingDB/blazingdb-ral.git
+
+git clone https://github.com/BlazingDB/blazingdb-orchestrator.git
+
+git clone https://github.com/BlazingDB/pyblazing.git
+
+git clone https://github.com/BlazingDB/blazingdb-io.git
+
+git clone https://github.com/BlazingDB/blazingdb-communication.git
+
+git clone https://github.com/BlazingDB/blazingdb-protocol.git
