aba74b86f1699d2216f3e93fc99f723b82d8ccde
felipe <felipe@blazingdb.com>
2019-08-21 17:04:13 -0500

    made some changes to add pyblazing

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -2,8 +2,8 @@
 
 cd $CONDA_PREFIX
 #set branch to latest to just build develop
-repos=(blazingdb-protocol blazingdb-communication blazingdb-io blazingdb-orchestrator blazingdb-ral blazingdb-calcite)
-branches=(feature/conda feature/conda feature/conda feature/conda feature/conda latest)
+repos=(blazingdb-protocol blazingdb-communication blazingdb-io blazingdb-orchestrator blazingdb-ral pyBlazing blazingdb-calcite)
+branches=(feature/conda feature/conda feature/conda feature/conda feature/conda feaure/conda latest)
 #assumes that you have installed blazingsql-dev into the current conda Environment
 
 i=0
@@ -12,7 +12,7 @@ do
   cd $CONDA_PREFIX
   if [ ! -d "$repo" ]; then
     git clone https://github.com/BlazingDB/$repo
-  else 
+  else
     cd $repo
     if [ ! -d ".git" ]; then # the folder existed but its not a repo. Lets delete it and actually get the repo
       cd ..
@@ -20,7 +20,7 @@ do
       git clone https://github.com/BlazingDB/$repo
     else
       cd ..
-    fi    
+    fi
   fi
   cd $repo
   if [ ${branches[i]} != "latest" ]; then
