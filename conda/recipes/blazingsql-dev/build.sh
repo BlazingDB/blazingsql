78b3c46818a06ca9dec27fe8d99ecb8a11493a5d
felipe <felipe@blazingdb.com>
2019-08-22 16:54:02 -0500

    made changes to blazing dev environment

--- conda/recipes/blazingsql-dev/build.sh
+++ conda/recipes/blazingsql-dev/build.sh
@@ -3,3 +3,12 @@
 
 INSTALL_PREFIX=${INSTALL_PREFIX:=${PREFIX:=${CONDA_PREFIX}}}
 cd ${INSTALL_PREFIX}
+
+echo -e "
+#!/bin/bash
+cd $CONDA_PREFIX
+git clone https://github.com/BlazingDB/pyBlazing.git
+pyBlazing/scripts/build-all.sh
+" > build-repos.sh
+
+chmod +x build-repos.sh 
