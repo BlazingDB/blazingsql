3a80c301cf2a7671fe1b258a68e5dea49d86ca6b
William Malpica <william@blazingdb.com>
2019-08-28 16:05:29 -0500

    fixed bad branch name

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -3,7 +3,7 @@
 cd $CONDA_PREFIX
 #set branch to latest to just build develop
 repos=(blazingdb-protocol blazingdb-communication blazingdb-io blazingdb-orchestrator blazingdb-ral pyBlazing blazingdb-calcite)
-branches=(feature/conda feature/conda feature/conda feature/conda feature/conda feaure/conda latest)
+branches=(feature/conda feature/conda feature/conda feature/conda feature/conda feature/conda latest)
 
 #assumes that you have installed blazingsql-dev into the current conda Environment
 
