66c2bdf522e91bae8b18d178df362a5ff1af6c84
Mario Inga <mario21ic@gmail.com>
2019-09-03 11:15:26 -0500

    feat(conda) adding channels to conda build

--- conda-build.sh
+++ conda-build.sh
@@ -3,4 +3,4 @@
 # example ./conda-build.sh 10.0|9.2     3.7|3.6        123
 
 export BUILD=$3
-conda build -c conda-forge -c defaults --python=$2 --output-folder $PWD/cuda$1_py$2/ .
+conda build -c conda-forge -c defaults -c rapidsai -c blazingsql --python=$2 --output-folder $PWD/cuda$1_py$2/ .
