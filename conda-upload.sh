e585772d232749a3972e40b323c124e6832b9fd8
Mario Inga <mario21ic@gmail.com>
2019-09-09 09:57:25 -0500

    feat(conda) upload: adding param force

--- conda-upload.sh
+++ conda-upload.sh
@@ -2,4 +2,4 @@
 # usage: ./conda-upload.sh user pass label path_file
 
 anaconda login --username $1 --password $2
-anaconda upload --user blazingsql --label cuda$3 $4
+anaconda upload --user blazingsql --label cuda$3 $4 --force
