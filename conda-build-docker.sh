f8a4b39d14413e77f27cc2b54f3cd31ef9b5dc85
editaxz <edit44@outlook.com>
2019-09-10 18:31:07 -0500

    adding feature to build

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # usage:   ./conda-build-docker.sh cuda_version python_version
 # example: ./conda-build-docker.sh 9.2|10.0 3.6|3.7
+export WORKSPACE=$PWD
 
-docker run --rm -u $(id -u):$(id -g) -v $PWD/:/app/ blazingdb/blazingsql:conda-cuda$1 /app/conda-build.sh $1 $2
+CUDA_VERSION="9.2"
+if [ ! -z $1 ]; then
+    CUDA_VERSION=$1
+fi
+
+ PYTHON_VERSION="3.7"
+if [ ! -z $2 ]; then
+    PYTHON_VERSION=$2
+fi
+
+ docker run -e CUDA_VER=${CUDA_VERSION} -e PYTHON=$PYTHON_VERSION -e MY_UPLOAD_KEY=$3 --rm -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} gpuci/miniconda-cuda:${CUDA_VERSION}-devel-ubuntu16.04 ./ci/cpu/build.sh
