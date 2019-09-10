c007620b30e89cb76c761427360ac71d574b0499
editaxz <edit44@outlook.com>
2019-09-10 12:43:54 -0500

    fixing build pyblazing

--- ci/cpu/pyblazing/conda-build.sh
+++ ci/cpu/pyblazing/conda-build.sh
@@ -2,7 +2,7 @@
 
 echo "****************************************************"
 echo "conda build -c conda-forge -c defaults -c rapidsai -c blazingsql --python=$PYTHON conda/recipes/pyBlazing/"
-conda build -c conda-forge -c defaults -c rapidsai -c blazingsql --python=$PYTHON conda/recipes/pyBlazing/
+conda build -c conda-forge -c defaults -c rapidsai -c blazingsql/label/main --python=$PYTHON conda/recipes/pyBlazing/
 
 #conda build --label $label -c conda-forge -c $channel -c rapidsai --python=$python --output-folder $CONDA_PREFIX/blazing-build/py${python}_cuda${toolkit} .
 
