9a529aeb7d1a0f836943f3a4b9dc81f77a11050d
Mario Inga <mario21ic@gmail.com>
2019-09-10 02:33:48 -0500

    feat(conda) improving scripts and jenkinsfile

--- conda-upload.sh
+++ conda-upload.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# usage: ./conda-upload.sh user pass label path_file
+# usage: ./conda-upload.sh cuda_version python_version token
+# usage: ./conda-upload.sh 9.2|10.0 3.6|3.7 123
 
-anaconda login --username $1 --password $2
-anaconda upload --user blazingsql --label cuda$3 $4 --force
+export GIT_DESCRIBE_TAG=`git describe --abbrev=0 --tags`
+export GIT_DESCRIBE_NUMBER=`git rev-list ${GIT_DESCRIBE_TAG}..HEAD --count`
+export PATH_FILE=`conda build --python=$2 conda/recipes/pyBlazing/ --output-folder $PWD/cuda$1_py$2/ --output`
+
+anaconda -t $3 upload --user blazingsql --label main --label cuda$1 $PATH_FILE --force
