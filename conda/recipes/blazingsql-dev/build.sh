d80a5901b1fec2c930ddbc6a432cad6ad8a7b3cc
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-09-12 03:36:49 -0500

    Add blazingdb-toolchain to blazingsql-dev as temp dependency

--- conda/recipes/blazingsql-dev/build.sh
+++ conda/recipes/blazingsql-dev/build.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
 
-INSTALL_PREFIX=${INSTALL_PREFIX:=${PREFIX:=${CONDA_PREFIX}}}
-cd ${INSTALL_PREFIX}
-
-echo -e '
-#!/bin/bash
-cd $CONDA_PREFIX
-git clone https://github.com/BlazingDB/pyBlazing.git
-cd pyBlazing
-git checkout feature/conda
-cd ..
-pyBlazing/scripts/build-all.sh
-' > build-repos.sh
-
-chmod +x build-repos.sh
