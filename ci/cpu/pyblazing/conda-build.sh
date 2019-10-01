a4fad0df3058e56721b41986c0d80a88d896ab74
editaxz <edit44@outlook.com>
2019-10-01 00:20:46 -0500

    changing channel to nightly

--- ci/cpu/pyblazing/conda-build.sh
+++ ci/cpu/pyblazing/conda-build.sh
@@ -2,5 +2,5 @@
 
 echo "****************************************************"
 echo "conda build -c blazingsql/label/main -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/"
-conda build -c blazingsql/label/main -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/
+conda build -c blazingsql-nightly -c rapidsai-nightly -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/
 
