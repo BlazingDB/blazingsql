4bc1132d8beea3ea585e0448034a78f27fa720eb
William Malpica <william@blazingdb.com>
2019-10-03 14:15:09 -0500

    added BlazingContext ability to provide a folder to put process logs

--- pyblazing/apiv2/context.py
+++ pyblazing/apiv2/context.py
@@ -10,6 +10,7 @@ from .sql import SQL
 from .sql import ResultSet
 from .datasource import build_datasource
 import time
+import datetime
 import socket, errno
 import subprocess
 import os
@@ -32,11 +33,17 @@ def checkSocket(socketNum):
     return socket_free
 
 
-def runEngine(network_interface = 'lo', processes = None):
-    process = None
-    devnull = open(os.devnull, 'w')
+def runEngine(network_interface = 'lo', processes = None, log_path=None):
+    if log_path is not None:
+        process_stdout= open(log_path + "_stdout.log", "w")
+        process_stderr= open(log_path + "_stderr.log", "w")
+    else:
+        process_stdout = open(os.devnull, 'w')
+        process_stderr = open(os.devnull, 'w')
+
+    process = None    
     if(checkSocket(9001)):
-        process = subprocess.Popen(['blazingsql-engine', '1', '0' , '127.0.0.1', '9100', '127.0.0.1', '9001', '8891', network_interface], stdout = devnull, stderr = devnull)
+        process = subprocess.Popen(['blazingsql-engine', '1', '0' , '127.0.0.1', '9100', '127.0.0.1', '9001', '8891', network_interface], stdout = process_stdout, stderr = process_stderr)
     else:
         print("WARNING: blazingsql-engine was not automativally started, its probably already running")
 
@@ -45,18 +52,24 @@ def runEngine(network_interface = 'lo', processes = None):
     return processes
 
 
-def setupDask(dask_client):
-    dask_client.run(runEngine,network_interface = 'eth0', processes = None)
+def setupDask(dask_client, log_path=None):
+    dask_client.run(runEngine,network_interface = 'eth0', processes = None, log_path = log_path)
 
 
-def runAlgebra(processes = None):
-    process = None
-    devnull = open(os.devnull, 'w')
+def runAlgebra(processes = None, log_path=None):
+    if log_path is not None:
+        process_stdout= open(log_path + "_stdout.log", "w")
+        process_stderr= open(log_path + "_stderr.log", "w")
+    else:
+        process_stdout = open(os.devnull, 'w')
+        process_stderr = open(os.devnull, 'w')
+
+    process = None    
     if(checkSocket(8890)):
         if(os.getenv("CONDA_PREFIX") == None):
             process = subprocess.Popen(['java', '-jar', '/usr/local/lib/blazingsql-algebra.jar', '-p', '8890'])
         else:
-            process = subprocess.Popen(['java', '-jar', os.getenv("CONDA_PREFIX") + '/lib/blazingsql-algebra.jar', '-p', '8890'], stdout = devnull, stderr = devnull)
+            process = subprocess.Popen(['java', '-jar', os.getenv("CONDA_PREFIX") + '/lib/blazingsql-algebra.jar', '-p', '8890'], stdout = process_stdout, stderr = process_stderr)
     else:
         print("WARNING: blazingsql-algebra was not automativally started, its probably already running")
 
@@ -65,11 +78,17 @@ def runAlgebra(processes = None):
     return processes
 
 
-def runOrchestrator(processes = None):
-    process = None
-    devnull = open(os.devnull, 'w')
+def runOrchestrator(processes = None, log_path=None):
+    if log_path is not None:
+        process_stdout= open(log_path + "_stdout.log", "w")
+        process_stderr= open(log_path + "_stderr.log", "w")
+    else:
+        process_stdout = open(os.devnull, 'w')
+        process_stderr = open(os.devnull, 'w')
+
+    process = None    
     if(checkSocket(9100)):
-        process = subprocess.Popen(['blazingsql-orchestrator', '9100', '8889', '127.0.0.1', '8890'], stdout = devnull, stderr = devnull)
+        process = subprocess.Popen(['blazingsql-orchestrator', '9100', '8889', '127.0.0.1', '8890'], stdout = process_stdout, stderr = process_stderr)
     else:
         print("WARNING: blazingsql-orchestrator was not automativally started, its probably already running")
 
@@ -80,33 +99,55 @@ def runOrchestrator(processes = None):
 
 class BlazingContext(object):
 
-    def __init__(self, connection = 'localhost:8889', dask_client = None, run_orchestrator = True, run_engine = True, run_algebra = True, network_interface = 'lo', leave_processes_running = False):
+    def __init__(self, connection = 'localhost:8889', dask_client = None, run_orchestrator = True, run_engine = True, run_algebra = True, network_interface = 'lo', leave_processes_running = False, logs_destination = None):
         """
         :param connection: BlazingSQL cluster URL to connect to
             (e.g. 125.23.14.1:8889, blazingsql-gateway:7887).
         """
+
+        logs_folder = None
+        if logs_destination is not None:
+            logs_folder = logs_destination # right now logs would go into files in a folder, but evetually we can use the same variable to define a network address for logging to be sent to
+            try :
+                if not os.path.isabs(logs_folder)  and os.getenv("CONDA_PREFIX") is not None:  # lets manage relative paths
+                    logs_folder = os.path.join(os.getenv("CONDA_PREFIX"), logs_folder)   
+
+                if not os.path.exists(logs_folder): # if folder does not exist, lets create it
+                    os.mkdir(logs_folder)
+            except Exception as error:
+                print("WARNING: could not establish logs_folder. " + str(error))
+                logs_folder = None
+        
+        timestamp_str = str(datetime.datetime.now()).replace(' ','_')        
+
         processes = None
         if not leave_processes_running:
             processes = []
 
         if(dask_client is None):
             if run_orchestrator:
-                processes = runOrchestrator(processes = processes)
-                time.sleep(2)  # lets the orchestrator start before we start the other processes
+                path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_orchestrator")
+                processes = runOrchestrator(processes = processes, log_path = path)
+                time.sleep(3)  # lets the orchestrator start before we start the other processes
             if run_engine:
-                processes = runEngine(network_interface = network_interface, processes = processes)
+                path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_engine")
+                processes = runEngine(network_interface = network_interface, processes = processes, log_path = path)
             if run_algebra:
-                processes = runAlgebra(processes = processes)
+                path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_algebra")
+                processes = runAlgebra(processes = processes, log_path = path)
             if run_engine or run_algebra:
-                time.sleep(3)  # lets the engine and algebra processes start before we continue
+                time.sleep(4)  # lets the engine and algebra processes start before we continue
         else:
             if run_orchestrator:
-                processes = runOrchestrator(processes = processes)
-                time.sleep(1)  # lets the orchestrator start before we start the other processes
-            setupDask(dask_client)
+                path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_orchestrator")
+                processes = runOrchestrator(processes = processes, log_path = path)
+                time.sleep(3)  # lets the orchestrator start before we start the other processes
+            path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_engine")
+            setupDask(dask_client, log_path = path)
             if run_algebra:
-                processes = runAlgebra(processes=processes)
-                time.sleep(3) # lets the engine and algebra processes start before we continue
+                path = None if logs_folder is None else os.path.join(logs_folder, timestamp_str + "_blazingsql_algebra")
+                processes = runAlgebra(processes=processes, log_path = path)
+                time.sleep(4) # lets the engine and algebra processes start before we continue
 
         # NOTE ("//"+) is a neat trick to handle ip:port cases
         parse_result = urlparse("//" + connection)
