0e05705082a2786a130bd489e86f62e5fbca61fb
editaxz <edit44@outlook.com>
2019-10-10 12:30:11 -0500

    updating nigtly

--- ci/cpu/pyblazing/conda-build.sh
+++ ci/cpu/pyblazing/conda-build.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
 
+# Nightly seccion
+echo "IS_NIGHTLY" $IS_NIGHTLY
+if [ $IS_NIGHTLY == "true" ]; then
+      NIGHTLY="-nightly"
+     # CUDF="cudf=0.10"
+
+      libcudf="libcudf=0.10"
+      nvstrings="nvstrings=0.10"
+      librmm="librmm=0.10"
+      daskcudf="dask-cudf=0.10"
+      #Replazing cudf version
+
+      echo "Replacing cudf version into meta.yaml"
+      sed -ie "s/libcudf/$libcudf/g" conda/recipes/pyBlazing/meta.yaml
+      sed -ie "s/nvstrings/$nvstrings/g" conda/recipes/pyBlazing/meta.yaml
+      sed -ie "s/librmm/$librmm/g" conda/recipes/pyBlazing/meta.yaml
+      sed -ie "s/dask-cudf/$daskcudf/g" conda/recipes/pyBlazing/meta.yaml
+fi
+
 conda build -c blazingsql${NIGHTLY} -c rapidsai${NIGHTLY} -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/
 
