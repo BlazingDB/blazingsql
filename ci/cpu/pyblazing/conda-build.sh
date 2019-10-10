60e02965565dbd93708e746531596dedad20d565
editaxz <edit44@outlook.com>
2019-10-10 15:03:09 -0500

    fix rmm

--- ci/cpu/pyblazing/conda-build.sh
+++ ci/cpu/pyblazing/conda-build.sh
@@ -8,14 +8,14 @@ if [ $IS_NIGHTLY == "true" ]; then
 
       libcudf="libcudf=0.10"
       nvstrings="nvstrings=0.10"
-      librmm="librmm=0.10"
+      rmm="rmm=0.10"
       daskcudf="dask-cudf=0.10"
       #Replazing cudf version
 
       echo "Replacing cudf version into meta.yaml"
       sed -ie "s/libcudf/$libcudf/g" conda/recipes/pyBlazing/meta.yaml
       sed -ie "s/nvstrings/$nvstrings/g" conda/recipes/pyBlazing/meta.yaml
-      sed -ie "s/librmm/$librmm/g" conda/recipes/pyBlazing/meta.yaml
+      sed -ie "s/rmm/$rmm/g" conda/recipes/pyBlazing/meta.yaml
       sed -ie "s/dask-cudf/$daskcudf/g" conda/recipes/pyBlazing/meta.yaml
 fi
 
