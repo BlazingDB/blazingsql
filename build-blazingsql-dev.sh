0b352413e536a18ec7f5a41f5418e0929d96815f
Mario Inga <mario21ic@gmail.com>
2019-10-25 13:53:19 -0500

    feat(conda) blazingsql-dev: adding script to build

---
+++ build-blazingsql-dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# usage:   ./conda-build-docker.sh cuda_version python_version token nightly
+# example: ./conda-build-docker.sh 9.2|10.0 3.6|3.7 123 true
+export WORKSPACE=$PWD
+
+CUDA_VERSION="9.2"
+if [ ! -z $1 ]; then
+    CUDA_VERSION=$1
+fi
+
+ PYTHON_VERSION="3.7"
+if [ ! -z $2 ]; then
+    PYTHON_VERSION=$2
+fi
+
+docker run -e CUDA_VER=${CUDA_VERSION} -e PYTHON=$PYTHON_VERSION -e MY_UPLOAD_KEY=$3 -e IS_NIGHTLY=$4 --rm -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} gpuci/miniconda-cuda:${CUDA_VERSION}-devel-ubuntu16.04 ./ci/cpu/build-dev.sh
