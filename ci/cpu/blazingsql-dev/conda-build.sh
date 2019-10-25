65ac2db2aef71dc6408656ac77084f134f46203b
Mario Inga <mario21ic@gmail.com>
2019-10-25 13:58:37 -0500

    fix(conda) blazingsql-dev: setting channel blazingsql main

--- ci/cpu/blazingsql-dev/conda-build.sh
+++ ci/cpu/blazingsql-dev/conda-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-conda build -c blazingsql/label/main -c blazingsql${NIGHTLY} -c rapidsai${NIGHTLY} -c conda-forge -c defaults --python=$PYTHON conda/recipes/blazingsql-dev/
+conda build -c blazingsql${NIGHTLY}/label/main -c rapidsai${NIGHTLY} -c conda-forge -c defaults --python=$PYTHON conda/recipes/blazingsql-dev/
