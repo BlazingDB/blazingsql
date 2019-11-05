85b0bb63e0b7dcd4c64dcbaf604e94b5839b7899
Mario Inga <mario21ic@gmail.com>
2019-11-05 15:17:20 -0500

    feat(conda) allowing to upload conda package to another repository

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# usage:   ./conda-build-docker.sh conda_build conda_upload python_version token
-# example: ./conda-build-docker.sh blazingsql-nightly blazingsql-nightly 9.2|10.0 3.6|3.7 123
+# usage:   ./conda-build-docker.sh python_version conda_build conda_upload token
+# example: ./conda-build-docker.sh 36|3.7 blazingsql-nightly blazingsql-nightly 123
 export WORKSPACE=$PWD
 
 PYTHON_VERSION="3.7"
 if [ ! -z $1 ]; then
-    PYTHON_VERSION=$3
+    PYTHON_VERSION=$1
 fi
 
-docker run --rm -e CONDA_BUILD=$1 -e CONDA_UPLOAD=$2 -e PYTHON=$PYTHON_VERSION -e MY_UPLOAD_KEY=$4 -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} gpuci/miniconda-cuda:10.0-devel-ubuntu16.04 ./ci/cpu/build.sh
+docker run --rm -e PYTHON=$PYTHON_VERSION -e CONDA_BUILD=$2 -e CONDA_UPLOAD=$3 -e MY_UPLOAD_KEY=$4 -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} gpuci/miniconda-cuda:10.0-devel-ubuntu16.04 ./ci/cpu/build.sh
