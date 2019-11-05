53efeb586eeef4da11bdfbd62ff75529a297ab7e
Mario Inga <mario21ic@gmail.com>
2019-11-05 17:03:25 -0500

    fix(conda) activating cuda version

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
-# usage:   ./conda-build-docker.sh python_version conda_build conda_upload token
-# example: ./conda-build-docker.sh 36|3.7 blazingsql-nightly blazingsql-nightly 123
+# usage:   ./conda-build-docker.sh cuda_version python_version conda_build conda_upload token
+# example: ./conda-build-docker.sh 9.2|10.0 3.6|3.7 blazingsql-nightly blazingsql-nightly 123
 export WORKSPACE=$PWD
 
-PYTHON_VERSION="3.7"
+CUDA_VERSION="9.2"
 if [ ! -z $1 ]; then
-    PYTHON_VERSION=$1
+    CUDA_VERSION=$1
 fi
 
-docker run --rm -e PYTHON=$PYTHON_VERSION -e CONDA_BUILD=$2 -e CONDA_UPLOAD=$3 -e MY_UPLOAD_KEY=$4 -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} gpuci/miniconda-cuda:10.0-devel-ubuntu16.04 ./ci/cpu/build.sh
+PYTHON_VERSION="3.7"
+if [ ! -z $2 ]; then
+    PYTHON_VERSION=$2
+fi
+
+docker run --rm \
+    -e CUDA_VER=${CUDA_VERSION} -e PYTHON=$PYTHON_VERSION \
+    -e CONDA_BUILD=$3 -e CONDA_UPLOAD=$4 -e MY_UPLOAD_KEY=$5 \
+    -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} \
+    gpuci/miniconda-cuda:${CUDA_VERSION}-devel-ubuntu16.04 \
+    ./ci/cpu/build.sh
