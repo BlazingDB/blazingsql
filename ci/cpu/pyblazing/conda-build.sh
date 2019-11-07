5e47eb07cfc2e9fa08733d025e823c49fc64fbd4
Mario Inga <mario21ic@gmail.com>
2019-11-07 17:48:45 -0500

    feat(conda) fix conda

--- ci/cpu/pyblazing/conda-build.sh
+++ ci/cpu/pyblazing/conda-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "CMD: conda build ${CONDA_CH} -c blazingsql -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/"
-conda build ${CONDA_CH} -c blazingsql -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/
+echo "CMD: conda build ${CONDA_CH} -c blazingsql/label/main/ -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/"
+conda build ${CONDA_CH} -c blazingsql/label/main/ -c rapidsai -c conda-forge -c defaults --python=$PYTHON conda/recipes/pyBlazing/
 
