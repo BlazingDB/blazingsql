d5887ae9961de2aa95566c4b55ba4b742605dd30
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2019-11-17 08:47:02 -0500

    fix conda build

--- setup.py
+++ setup.py
@@ -2,10 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='blazingsql',
-    version='0.1',
-    author='BlazingDB',
-    packages=find_packages(),
-    install_requires=(
-    ),
+    version="0.6",
+    description="BlazingSQL engine",
+    url="https://github.com/BlazingDB/pyBlazing/",
+    author="BlazingSQL",
+    license="Apache 2.0",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Topic :: Database",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=find_packages(include=["pyblazing", "pyblazing.*", "blazingsql", "blazingsql.*"]),
+    install_requires=(),
     zip_safe=False,
 )
