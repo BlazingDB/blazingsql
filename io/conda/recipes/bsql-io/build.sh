7fd18751b9f58ea7ddc5711135d92f011b6b209a
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2019-12-12 11:34:06 -0500

    Add unit tests support for io module

--- io/conda/recipes/bsql-io/build.sh
+++ io/conda/recipes/bsql-io/build.sh
@@ -12,11 +12,21 @@ if [ ! -z $2 ]; then
   build_type=$2
 fi
 
+run_test="ON"
+if [ ! -z $3 ]; then
+  run_test=$3
+fi
+
 mkdir -p build
 cd build
 
 echo "cmake .. -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}"
 cmake .. -DCMAKE_BUILD_TYPE=$build_type -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX}
 
-echo "make -j install"
-make -j install
+if [ "$run_test" == "ON" ]; then
+  echo "make -j all && make -j install && ctest"
+  make -j all && make -j install && ctest
+else
+  echo "make -j install"
+  make -j install
+fi
