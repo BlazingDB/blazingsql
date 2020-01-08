e4de9f80bb9ffea3be80c34473ac0b56bb5bfe1e
felipe <felipe@blazingdb.com>
2020-01-08 11:55:04 -0600

    added join function

--- engine/src/execution_graph/logic_controllers/LogicalFilter.h
+++ engine/src/execution_graph/logic_controllers/LogicalFilter.h
@@ -8,6 +8,7 @@ namespace ral{
 namespace processor{
 
 
+
 /**
 Takes a table and applies a boolean filter to it
 */
@@ -15,14 +16,27 @@ std::unique_ptr<ral::frame::BlazingTable> applyBooleanFilter(
   const ral::frame::BlazingTableView & table,
   const CudfColumnView boolValues);
 
+std::vector<std::unique_ptr<ral::frame::BlazingTable> > hashPartition(
+    const ral::frame::BlazingTableView & table,
+    std::vector<cudf::size_type> const& columns_to_hash,
+    int numPartitions);
+
+
+
 
 std::unique_ptr<ral::frame::BlazingTable> evaluateExpression(
   const ral::frame::BlazingTableView & table,
   const std::string & expression);
 
+std::unique_ptr<ral::frame::BlazingTable> processJoin(
+  const ral::frame::BlazingTableView & table_left,
+  const ral::frame::BlazingTableView & table_right,
+  const std::string & expression);
+
+
 /**
 Should be able to perform all 3 types of aggregations only locally what comes in
-as a solid chunk, inputs should be columns to aggregate, aggregation type, 
+as a solid chunk, inputs should be columns to aggregate, aggregation type,
 columns to group by
 */
 std::unique_ptr<ral::frame::BlazingTable> computeAggregation(
