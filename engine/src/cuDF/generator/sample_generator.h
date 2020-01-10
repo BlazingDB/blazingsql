2d2876a6dee1e8c202fa7399c3557bfedea878ce
gcca <cristhian@blazingdb.com>
2020-01-10 17:55:53 -0500

    [feature/sampling-cudf012] Migrate sampling generator to cudf 0.12

--- engine/src/cuDF/generator/sample_generator.h
+++ engine/src/cuDF/generator/sample_generator.h
@@ -3,12 +3,17 @@
 #include "GDFColumn.cuh"
 #include <vector>
 
+#include <execution_graph/logic_controllers/LogicPrimitives.h>
+
 namespace cudf {
 namespace generator {
 
 gdf_error generate_sample(const std::vector<gdf_column_cpp> & data_frame,
 	std::vector<gdf_column_cpp> & sampled_data,
 	cudf::size_type num_samples);
+
+std::unique_ptr<ral::frame::BlazingTable> generate_sample(
+	const ral::frame::BlazingTableView & blazingTableView, std::size_t num_samples);
 
 }  // namespace generator
 }  // namespace cudf
