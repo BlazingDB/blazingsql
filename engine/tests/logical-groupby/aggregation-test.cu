0a41e433a7f092112232a6ed76da04bc2ac5fdc2
Rommel <rommel@blazingdb.com>
2020-01-10 15:07:03 -0500

    Adding test with nulls for groupby wo aggs

--- engine/tests/logical-groupby/aggregation-test.cu
+++ engine/tests/logical-groupby/aggregation-test.cu
@@ -51,3 +51,39 @@ TYPED_TEST(AggregationTest, GroupbyWithoutAggs) {
 		cudf::test::expect_tables_equal(expect_cudf_table_view, table_out->view());
 	}
 }
+
+TYPED_TEST(AggregationTest, GroupbyWithoutAggsWithNull) {
+
+	using T = TypeParam;
+
+	cudf::test::fixed_width_column_wrapper<T> col1{{1, 1, 2, 1, 2, 2, 3, 3}, {1, 1, 1, 1, 1, 1, 1, 1}};
+	cudf::test::fixed_width_column_wrapper<T> col2{{1, 1, 9, 2, 2, 2, 9, 3}, {1, 1, 0, 1, 1, 1, 0, 1}};
+	cudf::test::fixed_width_column_wrapper<T> col3{{3, 4, 2, 9, 1, 1, 1, 3}, {1, 1, 1, 0, 1, 1, 1, 1}};
+	
+	cudf::table_view cudf_table_in_view {{col1, col2, col3}};
+
+	std::vector<std::string> names({"A", "B", "C"});
+	ral::frame::BlazingTableView table(cudf_table_in_view, names);
+
+	std::vector<int> group_column_indices{0, 1};
+
+	std::unique_ptr<ral::frame::BlazingTable> table_out = ral::operators::_new_groupby_without_aggregations(table, group_column_indices);
+
+	if (std::is_same<T, cudf::experimental::bool8>::value) {
+		cudf::test::fixed_width_column_wrapper<T> expect_col1{std::initializer_list<T> {1, 1}, {true, true}};
+		cudf::test::fixed_width_column_wrapper<T> expect_col2{std::initializer_list<T> {0, 1}, {false, true}};
+		cudf::test::fixed_width_column_wrapper<T> expect_col3{std::initializer_list<T> {1, 1}, {true, true}};
+
+		CudfTableView expect_cudf_table_view {{expect_col1, expect_col2, expect_col3}};
+
+		cudf::test::expect_tables_equal(expect_cudf_table_view, table_out->view());
+	} else {
+		cudf::test::fixed_width_column_wrapper<T> expect_col1{{1, 1, 2, 2, 3, 3}, {1, 1, 1, 1, 1, 1}};
+		cudf::test::fixed_width_column_wrapper<T> expect_col2{{1, 2, 9, 2, 9, 3}, {1, 1, 0, 1, 0, 1}};
+		cudf::test::fixed_width_column_wrapper<T> expect_col3{{3, 9, 2, 1, 1, 3}, {1, 0, 1, 1, 1, 1}};
+
+		CudfTableView expect_cudf_table_view {{expect_col1, expect_col2, expect_col3}};
+
+		cudf::test::expect_tables_equal(expect_cudf_table_view, table_out->view());
+	}
+}
