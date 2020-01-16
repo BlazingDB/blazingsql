0b9f293dbc776cf252822937d53fd70c17ec5674
Dillon Cullinan <dcullinan@dgx01.aselab.nvidia.com>
2020-01-16 12:28:43 -0800

    Test PREFIX

--- build.sh
+++ build.sh
@@ -53,7 +53,7 @@ TESTS="ON"
 # Set defaults for vars that may not have been defined externally
 #  FIXME: if INSTALL_PREFIX is not set, check PREFIX, then check
 #         CONDA_PREFIX, but there is no fallback from there!
-INSTALL_PREFIX=${INSTALL_PREFIX:=${PREFIX:=${CONDA_PREFIX}}}
+INSTALL_PREFIX=${INSTALL_PREFIX:=${CONDA_PREFIX}}
 PARALLEL_LEVEL=${PARALLEL_LEVEL:=""}
 
 function hasArg {
