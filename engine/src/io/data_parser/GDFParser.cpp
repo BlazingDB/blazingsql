d7107da3204c87a68962555bdcaa32f609901cab
christian <ccce91@gmail.com>
2020-01-17 17:31:38 -0500

    This version doesn't have problems

--- engine/src/io/data_parser/GDFParser.cpp
+++ engine/src/io/data_parser/GDFParser.cpp
@@ -67,25 +67,36 @@ std::unique_ptr<ral::frame::BlazingTable> gdf_parser::parse(
 	const std::string & user_readable_file_handle,
 	const Schema & schema,
 	std::vector<size_t> column_indices) {
-		// TODO: cordova Implements the new GDF parser with 0.12 API
-		return nullptr;
+
+	if(column_indices.size() == 0) {  // including all columns by default
+		column_indices.resize(schema.get_num_columns());
+		std::iota(column_indices.begin(), column_indices.end(), 0);
+	}
+
+	//cudf_io::read_orc_args orc_args = this->orc_args;
+
+	/*
+	if (column_indices.size() > 0) {
+
+		//const std::string column_name = this->tableSchema.names[column_index];
+		for (size_t column_i = 0; column_i < column_indices.size(); column_i++) {
+			orc_args.columns[column_i] = schema.get_name(column_indices[column_i]);
+		}
+
+		cudf_io::table_with_metadata orc_table = get_new_orc(orc_args, file);
+
+		if (orc_table.tbl->num_columns() <= 0)
+			Library::Logging::Logger().logWarn("orc_parser::parse no columns were read");
+		
+		return std::make_unique<ral::frame::BlazingTable>(std::move(orc_table.tbl), orc_table.metadata.column_names);
+	}
+	*/
+	// TODO: cordova Implements the new GDF parser with 0.12 API
+	return nullptr;
 }
 
 void gdf_parser::parse_schema(
 	std::vector<std::shared_ptr<arrow::io::RandomAccessFile>> files, ral::io::Schema & schema) {
-	std::vector<std::string> names;
-	std::vector<cudf::type_id> types;
-
-	// TODO percy cudf0.12 port to cudf::column
-	//	std::for_each(
-//		this->tableSchema.columns.begin(), this->tableSchema.columns.end(), [&types](cudf::column * column) {
-//			types.push_back(column->type().id());
-//		});
-
-	names = this->tableSchema.names;
-	ral::io::Schema temp_schema(names, types);
-	schema = temp_schema;
-	// generate schema from message here
 }
 
 
---
+++ engine/tests/io-test/parse_gdf.cu
@@ -0,0 +1,95 @@
+#include "io/Schema.h"
+//#include "io/data_parser/GDFParser.h"   // ERROR when include that header
+#include "io/DataLoader.h"
+#include "io/data_provider/DummyProvider.h"
+
+#include <cudf/cudf.h>
+#include <cudf/types.hpp>
+#include <from_cudf/cpp_tests/utilities/base_fixture.hpp>
+#include <cudf/table/table_view.hpp>
+#include <from_cudf/cpp_tests/utilities/column_utilities.hpp>
+#include <cudf/utilities/type_dispatcher.hpp>
+#include <from_cudf/cpp_tests/utilities/type_lists.hpp>
+#include <from_cudf/cpp_tests/utilities/column_wrapper.hpp>
+#include <from_cudf/cpp_tests/utilities/table_utilities.hpp>
+#include <vector>
+#include <execution_graph/logic_controllers/LogicalFilter.h>
+#include <execution_graph/logic_controllers/LogicPrimitives.h>
+
+#include <cudf/io/functions.hpp>
+
+
+using namespace ral::frame;
+using namespace ral::processor;
+
+using blazingdb::manager::Context;
+using Node = blazingdb::transport::Node;
+
+
+template <typename T>
+struct GDFTest : public cudf::test::BaseFixture {};
+
+// TYPED_TEST_CASE will run only a type: int16_t
+// Here the type specified is defined by cudf::test::Types<int16_t>
+TYPED_TEST_CASE(GDFTest, cudf::test::Types<int16_t>);
+
+
+TYPED_TEST(GDFTest, multipleColumns)
+{
+    // TypeParam is the type defined by TYPED_TEST_CASE for a particular test run. Here we are giving TypeParam an alias T
+    using T = TypeParam;
+
+    // GDF data should be created here 
+
+
+    // this is basically the test process
+    
+    // CREAR UN TABLESCHEMA y pasarle data MANUALMENTE
+    //TableSchema tableSchema;
+    //auto parser = std::make_shared<ral::io::gdf_parser>(tableSchema);
+
+    auto provider = std::make_shared<ral::io::dummy_data_provider>();
+    
+    //ral::io::data_loader loader(parser, provider);
+
+    ral::io::Schema schema;
+    //loader.get_schema(schema, {});
+    
+    Context queryContext{0, std::vector<std::shared_ptr<Node>>(), std::shared_ptr<Node>(), ""};
+    //std::unique_ptr<BlazingTable> gdf_table = loader.load_data(&queryContext, {}, schema);
+
+    
+    /*
+    // Here we are creating the expected output
+    cudf::test::fixed_width_column_wrapper<T> expect_col1{{0, 1, 2, 3, 4}, {1, 1, 1, 1, 1}};
+    cudf::test::strings_column_wrapper expect_col2{{"AFRICA", "AMERICA", "ASIA", "EUROPE", "MIDDLE EAST"}, {1, 1, 1, 1, 1}};
+    cudf::test::strings_column_wrapper expect_col3{{
+        "lar deposits. blithely final packages cajole. regular waters are final requests. regular accounts are according to ",
+        "hs use ironic, even requests. s", 
+        "ges. thinly even pinto beans ca", 
+        "ly final courts cajole furiously final excuse",
+        "uickly special accounts cajole carefully blithely close requests. carefully final asymptotes haggle furiousl"},
+        {1, 1, 1, 1, 1}};
+    CudfTableView expect_cudf_table_view {{expect_col1,expect_col2, expect_col3 }};
+
+    // Here we are printing out the output we got and the expect_cudf_table_view (only necessary for debugging)
+    std::string orc_table_col_0 = cudf::test::to_string(orc_table->view().column(0), "|");
+    std::cout<<"orc_table_col_0: "<< orc_table_col_0 <<std::endl;
+    std::string col0_string_cudf_table = cudf::test::to_string(expect_cudf_table_view.column(0), "|");
+    std::cout<<"expect_column_0: "<< col0_string_cudf_table <<std::endl;
+
+    std::string orc_table_col_1 = cudf::test::to_string(orc_table->view().column(1), "|");
+    std::cout<<"orc_table_col_1: "<< orc_table_col_1 <<std::endl;
+    std::string col1_string_cudf_table = cudf::test::to_string(expect_cudf_table_view.column(1), "|");
+    std::cout<<"expect_column_1: "<< col1_string_cudf_table <<std::endl;
+
+    std::string orc_table_col_2 = cudf::test::to_string(orc_table->view().column(2), "|");
+    std::cout<<"orc_table_col_2: "<< orc_table_col_2 <<std::endl;
+    std::string col2_string_cudf_table = cudf::test::to_string(expect_cudf_table_view.column(2), "|");
+    std::cout<<"expect_column_2: "<< col2_string_cudf_table <<std::endl;
+
+    // Here we are validating the output
+    // TODO: c.cordova, values are equals but something weird happen with the expect_tables_equal
+    //cudf::test::expect_tables_equal(expect_cudf_table_view, orc_table->view());
+    */
+}
