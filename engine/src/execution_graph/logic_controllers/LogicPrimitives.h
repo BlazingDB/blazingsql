b883c69acb38647ddd7a07663916ca33dec0d2ec
William Malpica <william@blazingdb.com>
2020-01-21 19:00:45 -0600

    implemented TableViewPair in evaluate_split_query. Builds but not unit tests

--- engine/src/execution_graph/logic_controllers/LogicPrimitives.h
+++ engine/src/execution_graph/logic_controllers/LogicPrimitives.h
@@ -17,6 +17,7 @@ namespace ral {
 
 namespace frame {
 
+class BlazingTable;
 class BlazingTableView;
 
 class BlazingTable {
@@ -45,6 +46,7 @@ private:
 
 class BlazingTableView {
 public:
+	BlazingTableView();
 	BlazingTableView(CudfTableView table, std::vector<std::string> columnNames);
 	BlazingTableView(BlazingTableView const &) = default;
 	BlazingTableView(BlazingTableView &&) = default;
@@ -57,6 +59,8 @@ public:
 	cudf::column_view const & column(cudf::size_type column_index) const { return table.column(column_index); }
 
 	std::vector<std::string> names() const;
+	// set columnNames
+	void setNames(const std::vector<std::string> &names) { this->columnNames = names; }
 
 	cudf::size_type num_columns() const { return table.num_columns(); }
 
@@ -68,6 +72,8 @@ private:
 	std::vector<std::string> columnNames;
 	CudfTableView table;
 };
+
+typedef std::pair<std::unique_ptr<ral::frame::BlazingTable>, ral::frame::BlazingTableView> TableViewPair;
 
 }  // namespace frame
 
