44ccc4e8204f1af1532845dea5f517be4ef8cb74
Alexander Ocsa <aocsa.cs@gmail.com>
2020-04-01 13:54:03 -0500

    [batching] minorfix

--- engine/src/execution_graph/logic_controllers/CacheMachine.cpp
+++ engine/src/execution_graph/logic_controllers/CacheMachine.cpp
@@ -140,6 +140,7 @@ void CacheMachine::addToCache(std::unique_ptr<ral::frame::BlazingTable> table, s
 	while(cacheIndex < memoryPerCache.size()) {
 		// TODO: BlazingMemoryResource::getUsedMemory() 
 		if((usedMemory[cacheIndex] + table->sizeInBytes() <= memoryPerCache[cacheIndex])) {
+			usedMemory[cacheIndex] += table->sizeInBytes();
 			if(cacheIndex == 0) {
 				// before we put into a cache, we need to make sure we fully own the table
 				auto column_names = table->names();
