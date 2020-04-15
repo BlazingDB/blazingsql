6221a7cd4c45cde3f187639ef15659724f729609
Alexander Ocsa <aocsa.cs@gmail.com>
2020-04-15 17:06:11 -0500

    [batching] minor refactor BlazingHostTable

--- engine/src/execution_graph/logic_controllers/BlazingHostTable.h
+++ engine/src/execution_graph/logic_controllers/BlazingHostTable.h
@@ -1,77 +1,46 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <blazingdb/transport/ColumnTransport.h>
-
-#include "cudf/column/column_view.hpp"
 #include "cudf/table/table.hpp"
-#include "cudf/table/table_view.hpp"
-#include <cudf/io/functions.hpp>
-#include <bmr/BlazingMemoryResource.h>
+
+namespace blazingdb {
+namespace transport {
+namespace experimental {
+
+struct ColumnTransport;
+
+}  // namespace experimental
+}  // namespace transport
+}  // namespace blazingdb
 
 namespace ral {
 namespace frame {
+
 using ColumnTransport = blazingdb::transport::experimental::ColumnTransport;
 
 class BlazingHostTable {
 public:
-    BlazingHostTable(const std::vector<ColumnTransport> &columns_offsets, std::vector<std::basic_string<char>> &&raw_buffers)
-            : columns_offsets{columns_offsets}, raw_buffers{std::move(raw_buffers)}
-    {
-        auto size = sizeInBytes();
-        blazing_host_memory_mesource::getInstance().allocate(size);
-    }
-    ~BlazingHostTable() {
-        auto size = sizeInBytes();
-        blazing_host_memory_mesource::getInstance().deallocate(size);
-    }
+    BlazingHostTable(const std::vector<ColumnTransport> &columns_offsets, std::vector<std::basic_string<char>> &&raw_buffers);
 
-    std::vector<cudf::data_type> get_schema() const {
-        std::vector<cudf::data_type> data_types(this->num_columns());
-        std::transform(columns_offsets.begin(), columns_offsets.end(), data_types.begin(), [](auto & col){
-            int32_t dtype = col.metadata.dtype;
-            return cudf::data_type{cudf::type_id(dtype)};
-        });
-        return data_types;
-    }
+    ~BlazingHostTable();
 
-    std::vector<std::string> names() const {
-        std::vector<std::string> col_names(this->num_columns());
-        std::transform(columns_offsets.begin(), columns_offsets.end(), col_names.begin(), [](auto & col){ return col.metadata.col_name; });
-        return col_names;
-    }
+    std::vector<cudf::data_type> get_schema() const;
 
-    cudf::size_type num_rows() const {
-        return columns_offsets.empty() ? 0 : columns_offsets.front().metadata.size;
-    }
+    std::vector<std::string> names() const;
 
-    cudf::size_type num_columns() const {
-        return columns_offsets.size();
-    }
+    cudf::size_type num_rows() const ;
 
-    unsigned long long sizeInBytes() {
-        unsigned long long total_size = 0L;
-        for (auto &col : columns_offsets) {
-            total_size += col.size_in_bytes;
-        }
-        return total_size;
-    }
+    cudf::size_type num_columns() const ;
 
-    void setPartitionId(const size_t &part_id) {
-        this->part_id = part_id;
-    }
+    unsigned long long sizeInBytes() ;
 
-    size_t get_part_id() {
-        return this->part_id;
-    }
+    void setPartitionId(const size_t &part_id) ;
 
-    const std::vector<ColumnTransport> & get_columns_offsets() const {
-        return columns_offsets;
-    }
+    size_t get_part_id() ;
 
-    const std::vector<std::basic_string<char>> & get_raw_buffers() const{
-        return raw_buffers;
-    }
+    const std::vector<ColumnTransport> & get_columns_offsets() const ;
+
+    const std::vector<std::basic_string<char>> & get_raw_buffers() const ;
 
 private:
     std::vector<ColumnTransport> columns_offsets;
