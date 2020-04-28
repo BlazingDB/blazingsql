ab80a1d7779d331bff467e8c1a82d97798755602
William Malpica <william@blazingdb.com>
2020-04-27 21:45:02 -0500

    implemented a semi-dynamic implementaiton of how much to partition for order by

--- engine/src/execution_graph/logic_controllers/PhysicalPlanGenerator.h
+++ engine/src/execution_graph/logic_controllers/PhysicalPlanGenerator.h
@@ -288,8 +288,9 @@ struct tree_processor {
 					query_graph += (*(child->kernel_unit))["output_b"] >> (*(parent->kernel_unit))["input_b"];
 				} else if ((child_kernel_type == kernel_type::PartitionKernel && parent_kernel_type == kernel_type::MergeStreamKernel)
 									|| (child_kernel_type == kernel_type::PartitionSingleNodeKernel && parent_kernel_type == kernel_type::MergeStreamKernel)) {
-					int num_partitions = this->context->getTotalNodes() * 4; // WSM TODO this is a hardcoded number for now. THis needs to change in the near future
-					auto cache_machine_config =	cache_settings{.type = CacheType::FOR_EACH, .num_partitions = num_partitions};
+					
+					const int MAX_NUM_ORDER_BY_PARTITIONS_PER_NODE = 8; // WSM TODO make this dynamic or configurable. ALso used in generate_distributed_partition_plan function
+					auto cache_machine_config =	cache_settings{.type = CacheType::FOR_EACH, .num_partitions = MAX_NUM_ORDER_BY_PARTITIONS_PER_NODE};
 					query_graph += link(*child->kernel_unit, *parent->kernel_unit, cache_machine_config);
 
 				} else {
