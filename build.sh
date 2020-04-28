5c9091e2077d98f44a945ca65e73dff82d2971dd
William Malpica <william@blazingdb.com>
2020-04-28 18:33:19 -0500

    changed build type

--- build.sh
+++ build.sh
@@ -47,7 +47,7 @@ BUILD_DIRS="${IO_BUILD_DIR} ${COMMS_BUILD_DIR} ${LIBENGINE_BUILD_DIR}"
 # Set defaults for vars modified by flags to this script
 VERBOSE=""
 QUIET="--quiet"
-BUILD_TYPE=Release
+BUILD_TYPE=RelWithDebInfo
 INSTALL_TARGET=install
 TESTS="ON"
 
