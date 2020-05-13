10770a48b074e41a1bd18ba9df4badfb8c5cdc43
Mario Inga <mario21ic@gmail.com>
2020-05-13 15:17:34 -0500

    feat(gpuci) adding custom label options to script to build locally

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# usage:   ./conda-build-docker.sh cuda_version python_version token conda_upload
-# example: ./conda-build-docker.sh 9.2|10.0|10.1 3.6|3.7 123 blazingsql-nightly
+# usage:   ./conda-build-docker.sh cuda_version python_version token custom_label conda_account
+# example: ./conda-build-docker.sh 10.0|10.2 3.6|3.7 123 beta blazingsql-nightly
 
 export WORKSPACE=$PWD
 
@@ -14,17 +14,30 @@ if [ ! -z $2 ]; then
   PYTHON_VERSION=$2
 fi
 
-CONDA_UPLOAD="blazingsql-nightly"
+MY_UPLOAD_KEY=""
+UPLOAD_BLAZING="0"
+if [ ! -z $3 ]; then
+  MY_UPLOAD_KEY=$3
+  UPLOAD_BLAZING=1
+fi
+
+CUSTOM_LABEL=""
 if [ ! -z $4 ]; then
-  CONDA_UPLOAD=$4
+  CUSTOM_LABEL=$4
+fi
+
+CONDA_USERNAME="blazingsql-nightly"
+if [ ! -z $5 ]; then
+  CONDA_USERNAME=$5
 fi
 
 docker run --rm -ti \
     --runtime=nvidia \
     -u $(id -u):$(id -g) \
     -e CUDA_VER=${CUDA_VERSION} -e PYTHON=$PYTHON_VERSION \
-    -e CONDA_UPLOAD=$CONDA_UPLOAD -e MY_UPLOAD_KEY=$3 \
-    -e UPLOAD_BLAZING=1 -e GIT_BRANCH="master" -e SOURCE_BRANCH="master" \
+    -e CONDA_USERNAME=$CONDA_USERNAME -e MY_UPLOAD_KEY=$MY_UPLOAD_KEY \
+    -e UPLOAD_BLAZING=$UPLOAD_BLAZING -e CUSTOM_LABEL=$CUSTOM_LABEL \
+    -e GIT_BRANCH="master" -e SOURCE_BRANCH="master" \
     -e WORKSPACE=$WORKSPACE \
     -v /etc/passwd:/etc/passwd \
     -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} \
