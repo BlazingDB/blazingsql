def574df9bcfd715c49a21ea003ec605000a8efd
Rommel <rommel@blazingdb.com>
2020-05-19 00:32:07 -0500

    Adding cache event logs for bindable

--- engine/src/execution_graph/logic_controllers/BatchProcessing.h
+++ engine/src/execution_graph/logic_controllers/BatchProcessing.h
@@ -312,7 +312,7 @@ public:
 									"ral_id"_a=context->getNodeIndex(ral::communication::CommunicationData::getInstance().getSelfNode()),
 									"query_id"_a=context->getContextToken(),
 									"source"_a=this->get_id(),
-									"sink"_a=this->get_message_id(),
+									"sink"_a=this->get_type_id(),
 									"num_rows"_a=num_rows,
 									"num_bytes"_a=num_bytes,
 									"event_type"_a="addCache",
@@ -404,7 +404,25 @@ public:
 											"timestamp_begin"_a=eventTimer.start_time(),
 											"timestamp_end"_a=eventTimer.end_time());
 
+							CodeTimer cacheEventTimer(false);
+
+							auto num_rows = columns->num_rows();
+							auto num_bytes = columns->sizeInBytes();
+
+							cacheEventTimer.start();
 							this->add_to_output_cache(std::move(columns));
+							cacheEventTimer.stop();
+
+							cache_events_logger->info("{ral_id}|{query_id}|{source}|{sink}|{num_rows}|{num_bytes}|{event_type}|{timestamp_begin}|{timestamp_end}",
+											"ral_id"_a=context->getNodeIndex(ral::communication::CommunicationData::getInstance().getSelfNode()),
+											"query_id"_a=context->getContextToken(),
+											"source"_a=this->get_id(),
+											"sink"_a=this->get_type_id(),
+											"num_rows"_a=num_rows,
+											"num_bytes"_a=num_bytes,
+											"event_type"_a="addCache",
+											"timestamp_begin"_a=cacheEventTimer.start_time(),
+											"timestamp_end"_a=cacheEventTimer.end_time());
 						}
 						else{
 							batch->setNames(fix_column_aliases(batch->names(), expression));
@@ -425,7 +443,25 @@ public:
 											"timestamp_begin"_a=eventTimer.start_time(),
 											"timestamp_end"_a=eventTimer.end_time());
 
+							CodeTimer cacheEventTimer(false);
+
+							auto num_rows = batch->num_rows();
+							auto num_bytes = batch->sizeInBytes();
+
+							cacheEventTimer.start();
 							this->add_to_output_cache(std::move(batch));
+							cacheEventTimer.stop();
+
+							cache_events_logger->info("{ral_id}|{query_id}|{source}|{sink}|{num_rows}|{num_bytes}|{event_type}|{timestamp_begin}|{timestamp_end}",
+											"ral_id"_a=context->getNodeIndex(ral::communication::CommunicationData::getInstance().getSelfNode()),
+											"query_id"_a=context->getContextToken(),
+											"source"_a=this->get_id(),
+											"sink"_a=this->get_type_id(),
+											"num_rows"_a=num_rows,
+											"num_bytes"_a=num_bytes,
+											"event_type"_a="addCache",
+											"timestamp_begin"_a=cacheEventTimer.start_time(),
+											"timestamp_end"_a=cacheEventTimer.end_time());
 						}
 					} catch(const std::exception& e) {
 						// TODO add retry here
