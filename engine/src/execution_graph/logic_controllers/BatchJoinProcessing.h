9dbc01ad91003e8dd4d91c8bfa8efc661804650c
christian <ccce91@gmail.com>
2020-06-24 16:50:43 -0500

    condition for cross join get before starts any join step

--- engine/src/execution_graph/logic_controllers/BatchJoinProcessing.h
+++ engine/src/execution_graph/logic_controllers/BatchJoinProcessing.h
@@ -214,7 +214,9 @@ public:
 	{
 		std::unique_ptr<CudfTable> result_table;
 		std::vector<std::pair<cudf::size_type, cudf::size_type>> columns_in_common;
-		if(this->join_type == INNER_JOIN) {
+
+		// cross join  (condition=[true])
+		if (condition == "true") {
 			//Removing nulls on key columns before joining
 			std::unique_ptr<CudfTable> table_left_dropna;
 			std::unique_ptr<CudfTable> table_right_dropna;
@@ -226,45 +228,57 @@ public:
 			if(has_nulls_right){
 				table_right_dropna = cudf::drop_nulls(table_right.view(), right_column_indices);
 			}
-			
-			// cross join
-			if (condition == "true") {
-				result_table = cudf::cross_join(
-					has_nulls_left ? table_left_dropna->view() : table_left.view(),
-					has_nulls_right ? table_right_dropna->view() : table_right.view());
-			} else {
+
+			result_table = cudf::cross_join(
+				has_nulls_left ? table_left_dropna->view() : table_left.view(),
+				has_nulls_right ? table_right_dropna->view() : table_right.view());
+		} else {
+			if(this->join_type == INNER_JOIN) {
+				//Removing nulls on key columns before joining
+				std::unique_ptr<CudfTable> table_left_dropna;
+				std::unique_ptr<CudfTable> table_right_dropna;
+				bool has_nulls_left = ral::processor::check_if_has_nulls(table_left.view(), left_column_indices);
+				bool has_nulls_right = ral::processor::check_if_has_nulls(table_right.view(), right_column_indices);
+				if(has_nulls_left){
+					table_left_dropna = cudf::drop_nulls(table_left.view(), left_column_indices);
+				}
+				if(has_nulls_right){
+					table_right_dropna = cudf::drop_nulls(table_right.view(), right_column_indices);
+				}
+				
 				result_table = cudf::inner_join(
 					has_nulls_left ? table_left_dropna->view() : table_left.view(),
 					has_nulls_right ? table_right_dropna->view() : table_right.view(),
 					this->left_column_indices,
 					this->right_column_indices,
 					columns_in_common);
-			}
-			
-		} else if(this->join_type == LEFT_JOIN) {
-			//Removing nulls on right key columns before joining
-			std::unique_ptr<CudfTable> table_right_dropna;
-			bool has_nulls_right = ral::processor::check_if_has_nulls(table_right.view(), right_column_indices);
-			if(has_nulls_right){
-				table_right_dropna = cudf::drop_nulls(table_right.view(), right_column_indices);
-			}
+				
+			} else if(this->join_type == LEFT_JOIN) {
+				//Removing nulls on right key columns before joining
+				std::unique_ptr<CudfTable> table_right_dropna;
+				bool has_nulls_right = ral::processor::check_if_has_nulls(table_right.view(), right_column_indices);
+				if(has_nulls_right){
+					table_right_dropna = cudf::drop_nulls(table_right.view(), right_column_indices);
+				}
 
-			result_table = cudf::left_join(
-				table_left.view(),
-				has_nulls_right ? table_right_dropna->view() : table_right.view(),
-				this->left_column_indices,
-				this->right_column_indices,
-				columns_in_common);
-		} else if(this->join_type == OUTER_JOIN) {
-			result_table = cudf::full_join(
-				table_left.view(),
-				table_right.view(),
-				this->left_column_indices,
-				this->right_column_indices,
-				columns_in_common);
-		} else {
-			RAL_FAIL("Unsupported join operator");
+				result_table = cudf::left_join(
+					table_left.view(),
+					has_nulls_right ? table_right_dropna->view() : table_right.view(),
+					this->left_column_indices,
+					this->right_column_indices,
+					columns_in_common);
+			} else if(this->join_type == OUTER_JOIN) {
+				result_table = cudf::full_join(
+					table_left.view(),
+					table_right.view(),
+					this->left_column_indices,
+					this->right_column_indices,
+					columns_in_common);
+			} else {
+				RAL_FAIL("Unsupported join operator");
+			}
 		}
+		
 		return std::make_unique<ral::frame::BlazingTable>(std::move(result_table), this->result_names);
 	}
 
