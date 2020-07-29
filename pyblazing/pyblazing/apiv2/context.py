43ddc9908f661816d27134ff38fafbef1fc0d7ff
William Malpica <william@blazingdb.com>
2020-07-29 16:47:03 -0500

    fixed issue due to apparent change in pandas API

--- pyblazing/pyblazing/apiv2/context.py
+++ pyblazing/pyblazing/apiv2/context.py
@@ -1787,7 +1787,7 @@ class BlazingContext(object):
                 for group_id in grouped.groups:
                     row_indices = grouped.groups[group_id].values.tolist()
                     row_meta_ids = metadata_ids["row_group_index"]
-                    row_groups_col = row_meta_ids.values.tolist()
+                    row_groups_col = row_meta_ids.tolist()
                     row_group_ids = [row_groups_col[i] for i in row_indices]
                     row_groups_ids.append(row_group_ids)
                 table.row_groups_ids = row_groups_ids
@@ -1961,8 +1961,7 @@ class BlazingContext(object):
                     if group_id < len(current_table.uri_values):
                         uri_values.append(current_table.uri_values[group_id])
                     row_groups_col = file_and_rowgroup_indices[
-                        "row_group_index"
-                    ].values.tolist()
+                        "row_group_index"].tolist()
                     row_group_ids = [row_groups_col[i] for i in row_indices]
                     row_groups_ids.append(row_group_ids)
 
