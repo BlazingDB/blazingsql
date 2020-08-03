0ba1f7dee45bf603acdacdc6476febb0e4691dcf
felipe <felipe@blazingdb.com>
2020-08-03 14:55:59 -0500

    added pip requirements and running doxygen to build

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,14 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+#run doxygen for read the docs
+import subprocess, os
+
+read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+
+if read_the_docs_build:
+
+    subprocess.call('cd ../doxygen; doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
