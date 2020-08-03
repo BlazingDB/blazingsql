780785a1198a91a886d3eb17a47080cee746f364
felipeblazing <felipe@blazingdb.com>
2020-08-03 15:04:19 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('doxygen', shell=True)
 
 # -- Project information -----------------------------------------------------
 
