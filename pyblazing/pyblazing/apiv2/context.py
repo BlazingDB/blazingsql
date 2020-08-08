3b491a068306c05dbda633e26317e527d3c09301
kharoly <kharoly.cs@gmail.com>
2020-08-08 01:11:59 -0500

    style

--- pyblazing/pyblazing/apiv2/context.py
+++ pyblazing/pyblazing/apiv2/context.py
@@ -81,7 +81,7 @@ RelConversionExceptionClass = jpype.JClass(
     "org.apache.calcite.tools.RelConversionException"
 )
 
-def checkSocket(socketNum):    
+def checkSocket(socketNum):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     socket_free = False
@@ -1422,16 +1422,16 @@ class BlazingContext(object):
         try:
             algebra = self.generator.getRelationalAlgebraString(sql)
 
-        except SqlValidationExceptionClass as exception: 
+        except SqlValidationExceptionClass as exception:
             # jpype.JException as exception:
-            raise Exception(exception.message()) 
+            raise Exception(exception.message())
             # algebra = ""
             # print("SQL Parsing Error")
             # print(exception.message())
         except SqlSyntaxExceptionClass as exception:
-            raise Exception(exception.message()) 
+            raise Exception(exception.message())
         except RelConversionExceptionClass as exception:
-            raise Exception(exception.message()) 
+            raise Exception(exception.message())
         # if algebra.startswith("fail:"):
         #     print("Error found")
         #     print(algebra)
