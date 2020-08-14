52762401cf463c4ce4fe5990f6bdec8f64ac47cd
Percy Camilo Triveño Aucahuasi <percy.camilo.ta@gmail.com>
2020-08-14 12:21:14 -0500

    use docker as debug env for centos

---
+++ powerpc/README.md
@@ -0,0 +1,14 @@
+## Readme
+
+### Building docker image for build:
+```
+./docker-build.sh
+```
+
+### Run a container
+```
+docker run -ti blazingsql bash
+```
+
+### Dockerfile
+nvidia-docker doesn't have powerpc support so this image is only for debugging
--- powerpc/install-dependencies.sh
+++ powerpc/install-dependencies.sh
@@ -1,1 +1,5 @@
 #!/bin/bash
+
+# 1) hsi/5.0.2.p5    4) darshan-runtime/3.1.7   7) cuda/10.1.243  10) spectrum-mpi/10.3.1.2-20200121
+# 2) xalt/1.2.0      5) DefApps                 8) gcc/7.4.0      11) boost/1.66.0
+# 3) lsf-tools/2.0   6) python/3.7.0            9) cmake/3.17.3
