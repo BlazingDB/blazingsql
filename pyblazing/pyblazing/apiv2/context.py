30ea85a19ea61efbf7d2e2adbc72991d14151558
christian <ccce91@gmail.com>
2020-08-24 10:28:16 -0500

    minor grammar

--- pyblazing/pyblazing/apiv2/context.py
+++ pyblazing/pyblazing/apiv2/context.py
@@ -1905,7 +1905,7 @@ class BlazingContext(object):
     def describe_table(self, table_name):
         """
         Returns a dictionary with the names of all the columns and their types
-        for the specified table. A ValueError is thrown if thr table is not found.
+        for the specified table. A ValueError is thrown if the table is not found.
 
         Parameters
         ----------
