82c21253b879b27e31741a329cf9cc69e57f05a5
christian <ccce91@gmail.com>
2020-10-08 16:51:01 -0500

    return found as requested

--- io/src/FileSystem/private/FileSystemManager_p.cpp
+++ io/src/FileSystem/private/FileSystemManager_p.cpp
@@ -79,7 +79,7 @@ bool FileSystemManager::Private::deregisterFileSystem(const std::string & author
 	}
 
 	if(found == false) {
-		return false;
+		return found;
 	}
 
 	const int fileSystemId = this->fileSystemIds[authority];
