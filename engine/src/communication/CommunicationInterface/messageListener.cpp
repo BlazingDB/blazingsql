636738ba25b808719f8f31f335de655c49e70d7f
William Malpica <william@blazingdb.com>
2020-10-22 14:41:28 -0500

    matching code in felipes branch

--- engine/src/communication/CommunicationInterface/messageListener.cpp
+++ engine/src/communication/CommunicationInterface/messageListener.cpp
@@ -158,7 +158,7 @@ void tcp_message_listener::start_polling(){
       int connection_fd;
       // TODO: be able to stop this thread from running when the engine is killed
       while((connection_fd = accept(socket_fd, (struct sockaddr *) &client_address, &len)) != -1) {
-        auto fwd = pool.push([this, connection_fd](int thread_num) {
+        pool.push([this, connection_fd](int thread_num) {
           CodeTimer timer;
           cudaStream_t stream;
           cudaStreamCreate(&stream);
@@ -170,7 +170,7 @@ void tcp_message_listener::start_polling(){
           auto meta_read_time = timer.elapsed_time();
           //status_code success = status_code::OK;
           //io::write_to_socket(connection_fd, &success, sizeof(success));
-		
+		{
           auto receiver = std::make_shared<message_receiver>(_nodes_info_map, data);
 
         //	std::cout<<"elapsed time make receiver done "<<timer.elapsed_time()<<std::endl;
@@ -206,6 +206,7 @@ void tcp_message_listener::start_polling(){
               cudaMemcpyAsync(buffer_chunk_start, pinned_buffer->data, chunk_size, cudaMemcpyHostToDevice, stream);
               pinned_buffers[chunk] = pinned_buffer;
             }
+
             //std::cout<<"elapsed copy from gpu before synch "<<timer_2.elapsed_time()<<std::endl;
             total_read_time += timer_2.elapsed_time();
             timer_2.reset();
@@ -220,6 +221,7 @@ void tcp_message_listener::start_polling(){
             }
             buffer_position++;
           }
+		close(connection_fd);
           auto duration = timer.elapsed_time();
           std::cout<<"Transfer duration before finish "<<duration <<" Throughput was "<<
           (( (float) total_size) / 1000000.0)/(((float) duration)/1000.0)<<" MB/s"<<std::endl;
@@ -232,7 +234,7 @@ void tcp_message_listener::start_polling(){
 		std::cout<<"META, Recevier, allocate, read, synchronize, total_before_finish, total_after_finish,bytes\n"<<
 		meta_read_time<<", "<<receiver_time<<", "<<total_allocate_time<<", "<<total_read_time<<","<<total_sync_time<<", "<<duration<<","<<duration_2<<", "<<total_size<<std::endl;
 
-		// close(connection_fd);
+		}
 		cudaStreamSynchronize(stream);
 		cudaStreamDestroy(stream);
         });
