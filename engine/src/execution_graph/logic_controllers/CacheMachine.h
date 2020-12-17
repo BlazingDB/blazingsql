d070552c912e414efb410cc2811c72743ee40324
William Malpica <william@blazingdb.com>
2020-12-17 15:30:58 -0600

    fixed log messge

--- engine/src/execution_graph/logic_controllers/CacheMachine.h
+++ engine/src/execution_graph/logic_controllers/CacheMachine.h
@@ -805,7 +805,7 @@ public:
 					auto logger = spdlog::get("batch_logger");
 					if(logger != nullptr) {
 						logger->warn("|||{info}|{duration}||||",
-											"info"_a="WaitingQueue wait_until_finished timed out",
+											"info"_a="WaitingQueue wait_until_num_bytes timed out",
 											"duration"_a=blazing_timer.elapsed_time());
 					}
 				}
