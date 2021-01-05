a0989c508cc532f75af08a47c6080c2bed1ed1b8
felipe <felipe@blazingsql>
2021-01-05 08:55:14 -0600

    added two outputs to the input_comms, one when the message begins and one when it finishes

--- engine/src/communication/CommunicationInterface/messageReceiver.cpp
+++ engine/src/communication/CommunicationInterface/messageReceiver.cpp
@@ -7,6 +7,8 @@ namespace comm {
 using namespace fmt::literals;
 message_receiver::message_receiver(const std::map<std::string, comm::node>& nodes, const std::vector<char> & buffer)
 {
+
+
   _nodes_info_map = nodes;
   auto metadata_and_transports = detail::get_metadata_and_transports_and_buffer_sizes_from_bytes(buffer);
   _metadata = std::get<0>(metadata_and_transports);
@@ -20,6 +22,19 @@ message_receiver::message_receiver(const std::map<std::string, comm::node>& node
                       graph->get_kernel_output_cache(kernel_id, cache_id) : graph->get_input_message_cache();
 //_metadata.print();
 _raw_buffers.resize(_buffer_sizes.size());
+  std::shared_ptr<spdlog::logger> comms_logger;
+	comms_logger = spdlog::get("input_comms");
+  auto destinations = _metadata.get_values()[ral::cache::WORKER_IDS_METADATA_LABEL];
+
+  comms_logger->info("{ral_id}|{query_id}|{kernel_id}|{dest_ral_id}|{dest_ral_count}|{dest_cache_id}|{message_id}|{phase}",
+  "ral_id"_a=_metadata.get_values()[ral::cache::RAL_ID_METADATA_LABEL],
+  "query_id"_a=_metadata.get_values()[ral::cache::QUERY_ID_METADATA_LABEL],
+  "kernel_id"_a=_metadata.get_values()[ral::cache::KERNEL_ID_METADATA_LABEL],
+  "dest_ral_id"_a=destinations, //false
+  "dest_ral_count"_a=std::count(destinations.begin(), destinations.end(), ',') + 1,
+  "dest_cache_id"_a=_metadata.get_values()[ral::cache::CACHE_ID_METADATA_LABEL],
+  "message_id"_a=_metadata.get_values()[ral::cache::MESSAGE_ID],
+  "phase"_a="begin");
 }
 
 size_t message_receiver::buffer_size(u_int16_t index){
@@ -65,14 +80,15 @@ void message_receiver::finish(cudaStream_t stream) {
 	comms_logger = spdlog::get("input_comms");
   auto destinations = _metadata.get_values()[ral::cache::WORKER_IDS_METADATA_LABEL];
 
-  comms_logger->info("{ral_id}|{query_id}|{kernel_id}|{dest_ral_id}|{dest_ral_count}|{dest_cache_id}|{message_id}",
+  comms_logger->info("{ral_id}|{query_id}|{kernel_id}|{dest_ral_id}|{dest_ral_count}|{dest_cache_id}|{message_id}|{phase}",
   "ral_id"_a=_metadata.get_values()[ral::cache::RAL_ID_METADATA_LABEL],
   "query_id"_a=_metadata.get_values()[ral::cache::QUERY_ID_METADATA_LABEL],
   "kernel_id"_a=_metadata.get_values()[ral::cache::KERNEL_ID_METADATA_LABEL],
   "dest_ral_id"_a=destinations, //false
   "dest_ral_count"_a=std::count(destinations.begin(), destinations.end(), ',') + 1,
   "dest_cache_id"_a=_metadata.get_values()[ral::cache::CACHE_ID_METADATA_LABEL],
-  "message_id"_a=_metadata.get_values()[ral::cache::MESSAGE_ID]);
+  "message_id"_a=_metadata.get_values()[ral::cache::MESSAGE_ID],
+  "phase"_a="end");
   
   std::unique_ptr<ral::cache::CacheData> table = 
       std::make_unique<ral::cache::CPUCacheData>(_column_transports,std::move(_raw_buffers),_metadata);
