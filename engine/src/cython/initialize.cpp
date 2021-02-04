f6647275ad3a8d32dbb95383a53a76c2e2895d1d
William Malpica <william@blazingdb.com>
2021-02-04 09:34:27 -0600

    refactored ucx code out of initialize.cpp. improved tcp send and receive test. Added new ucx test that does not work

--- engine/src/cython/initialize.cpp
+++ engine/src/cython/initialize.cpp
@@ -31,6 +31,7 @@
 #include "blazingdb/io/Library/Logging/ServiceLogging.h"
 #include <blazingdb/io/Util/StringUtil.h>
 
+#include "communication/ucx_init.h"
 #include "communication/CommunicationData.h"
 
 #include <bmr/initializer.h>
@@ -58,7 +59,7 @@ using namespace fmt::literals;
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
-#include <netdb.h>
+
 
 using namespace fmt::literals;
 using namespace ral::cache;
@@ -162,367 +163,6 @@ void create_logger(std::string fileName,
 	spdlog::flush_every(std::chrono::seconds(1));
 }
 
-//=============================================================================
-
-template <class Callable>
-static inline void CheckError(const bool condition,
-                              const std::string &message,
-                              Callable &&callable) {
-  if (condition) {
-    std::forward<Callable>(callable)();
-    std::cerr << message << std::endl;
-    throw std::runtime_error(message);
-  }
-}
-
-static inline void CheckError(const bool condition,
-                              const std::string &message) {
-  CheckError(condition, message, []() {});
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-class UcpWorkerAddress {
-public:
-  ucp_address_t *address;
-  std::size_t length;
-};
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-class AddressExchanger {
-public:
-  const UcpWorkerAddress Exchange(const UcpWorkerAddress &ucpWorkerAddress) {
-
-    std::uint8_t *data = new std::uint8_t[ucpWorkerAddress.length];
-
-    UcpWorkerAddress peerUcpWorkerAddress{
-        reinterpret_cast<ucp_address_t *>(data),
-        std::numeric_limits<decltype(ucpWorkerAddress.length)>::max()};
-
-    try {
-      Exchange(&peerUcpWorkerAddress.length,
-               fd(),
-               &ucpWorkerAddress.length,
-               sizeof(ucpWorkerAddress.length));
-
-      Exchange(peerUcpWorkerAddress.address,
-               fd(),
-               ucpWorkerAddress.address,
-               ucpWorkerAddress.length);
-    } catch (...) {
-      delete[] data;
-      throw;
-    }
-
-    return peerUcpWorkerAddress;
-  }
-
-  static std::unique_ptr<AddressExchanger>
-  MakeForSender(const std::uint16_t port);
-
-  static std::unique_ptr<AddressExchanger>
-  MakeForReceiver(const std::uint16_t port, const char *hostname);
-
-  virtual int fd() = 0;
-
-private:
-  static inline void Exchange(void *peerData,
-                              const int fd,
-                              const void *localData,
-                              const std::size_t length) {
-    int ret = send(fd, localData, length, 0);
-    CheckError(ret != static_cast<int>(length), "send");
-    ret = recv(fd, peerData, length, MSG_WAITALL);
-    CheckError(ret != static_cast<int>(length), "recv");
-  }
-};
-
-class AddressExchangerForSender : public AddressExchanger {
-public:
-  ~AddressExchangerForSender() {
-		closeCurrentConnection();
-		CheckError(close(lsock_), "close sender");
-	}
-
-  AddressExchangerForSender(const std::uint16_t port) {
-    struct sockaddr_in inaddr;
-
-    lsock_ = -1;
-    dsock_ = -1;
-    int optval = 1;
-    int ret;
-
-    lsock_ = socket(AF_INET, SOCK_STREAM, 0);
-    if (lsock_ < 0) {
-      std::cerr << "open server socket" << std::endl;
-      throw std::runtime_error("open server socket");
-    }
-
-    optval = 1;
-    ret = setsockopt(lsock_, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
-    if (ret < 0) {
-      std::cerr << "server setsockopt()" << std::endl;
-      close(lsock_);
-      throw std::runtime_error("server setsockopt()");
-    }
-
-    inaddr.sin_family = AF_INET;
-    inaddr.sin_port = htons(port);
-    inaddr.sin_addr.s_addr = INADDR_ANY;
-    std::memset(inaddr.sin_zero, 0, sizeof(inaddr.sin_zero));
-    ret = bind(lsock_, (struct sockaddr *) &inaddr, sizeof(inaddr));
-    if (ret < 0) {
-      close(lsock_);
-      throw std::runtime_error("bind server");
-    }
-
-    ret = listen(lsock_, SOMAXCONN);
-    if (ret < 0) {
-      close(lsock_);
-      throw std::runtime_error("listen server");
-    }
-
-
-    // dsock_ = accept(lsock_, NULL, NULL);
-    // if (dsock_ < 0) {
-    //   std::cout << "accept server" << std::endl;
-    //   close(lsock_);
-    //   throw std::runtime_error("accept server");
-    // }
-
-    // close(lsock_);
-
-    // CheckError(dsock_ < 0, "server_connect");
-  }
-
-	char *get_ip_str(const struct sockaddr *sa, char *s, size_t maxlen)
-	{
-			switch(sa->sa_family) {
-					case AF_INET:
-							inet_ntop(AF_INET, &(((struct sockaddr_in *)sa)->sin_addr),
-											s, maxlen);
-							break;
-
-					case AF_INET6:
-							inet_ntop(AF_INET6, &(((struct sockaddr_in6 *)sa)->sin6_addr),
-											s, maxlen);
-							break;
-
-					default:
-							strncpy(s, "Unknown AF", maxlen);
-							return NULL;
-			}
-
-			return s;
-	}
-
-	bool acceptConnection() {
-		struct sockaddr address;
-    unsigned int addrlen = sizeof(address);
-    dsock_ = accept(lsock_, &address, (socklen_t*)&addrlen);
-    if (dsock_ < 0) {
-      close(lsock_);
-      throw std::runtime_error("accept server");
-    }
-
-    CheckError(dsock_ < 0, "server_connect");
-
-		char str_buffer[INET6_ADDRSTRLEN];
-		get_ip_str(&address, str_buffer, INET6_ADDRSTRLEN);
-
-		return true;
-	}
-
-	void closeCurrentConnection(){
-		if (dsock_ != -1) {
-			CheckError(close(dsock_), "close sender fd");
-			dsock_ = -1;
-		}
-	}
-
-  int fd() final { return dsock_; }
-
-private:
-  int dsock_;
-	int lsock_;
-};
-
-class AddressExchangerForReceiver : public AddressExchanger {
-public:
-  ~AddressExchangerForReceiver() {
-		CheckError(close(fd()), "close receiver");
-	}
-
-  AddressExchangerForReceiver(const std::uint16_t port, const char *hostname) {
-    struct sockaddr_in conn_addr;
-    struct hostent *he;
-    int connfd;
-    int ret;
-
-    connfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (connfd < 0) {
-      const std::string message = "open client socket";
-      throw std::runtime_error(message);
-    }
-
-    he = gethostbyname(hostname);
-    if (he == NULL || he->h_addr_list == NULL) {
-      const std::string message = "found a host";
-      close(connfd);
-      throw std::runtime_error(message);
-    }
-
-    conn_addr.sin_family = he->h_addrtype;
-    conn_addr.sin_port = htons(port);
-
-    std::memcpy(&conn_addr.sin_addr, he->h_addr_list[0], he->h_length);
-    std::memset(conn_addr.sin_zero, 0, sizeof(conn_addr.sin_zero));
-
-
-    int num_attempts = 50;
-    int attempt = 0;
-    while (attempt < num_attempts){
-        ret = connect(connfd, (struct sockaddr *) &conn_addr, sizeof(conn_addr));
-        if (ret < 0) {
-            attempt++;
-		std::this_thread::sleep_for (std::chrono::seconds(1));
-        } else {
-		break;
-	}
-	if (attempt == num_attempts){
-	      const std::string message = "could not connect to client";
-	      close(connfd);
-	      throw std::runtime_error(message);
-	}
-    }
-
-    CheckError(connfd < 0, "server_connect");
-    connfd_ = connfd;
-  }
-
-  int fd() final { return connfd_; }
-
-private:
-  int connfd_;
-};
-
-std::unique_ptr<AddressExchanger>
-AddressExchanger::MakeForSender(const std::uint16_t port) {
-  return std::make_unique<AddressExchangerForSender>(port);
-}
-
-std::unique_ptr<AddressExchanger>
-AddressExchanger::MakeForReceiver(const std::uint16_t port,
-                                  const char *hostname) {
-  return std::make_unique<AddressExchangerForReceiver>(port, hostname);
-}
-
-static void request_init(void *request)
-{
-  struct comm::ucx_request *req = (comm::ucx_request *)request;
-  req->completed = 0;
-  req->uid = -1;
-}
-
-//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-ucp_context_h CreateUcpContext() {
-  ucp_config_t *config;
-  ucs_status_t status = ucp_config_read(NULL, NULL, &config);
-  CheckError(status != UCS_OK, "ucp_config_read");
-
-  ucp_params_t ucp_params;
-  std::memset(&ucp_params, 0, sizeof(ucp_params));
-  ucp_params.field_mask = UCP_PARAM_FIELD_FEATURES |
-                          UCP_PARAM_FIELD_REQUEST_SIZE |
-                          UCP_PARAM_FIELD_REQUEST_INIT;
-  ucp_params.features = UCP_FEATURE_TAG | UCP_FEATURE_WAKEUP;
-  ucp_params.request_size = sizeof(comm::ucx_request);
-  ucp_params.request_init = request_init;
-
-  ucp_context_h ucp_context;
-  status = ucp_init(&ucp_params, config, &ucp_context);
-
-  const bool hasPrintUcpConfig = false;
-  if (hasPrintUcpConfig) {
-    ucp_config_print(config, stdout, NULL, UCS_CONFIG_PRINT_CONFIG);
-  }
-
-  ucp_config_release(config);
-  CheckError(status != UCS_OK, "ucp_init");
-
-  return ucp_context;
-}
-
-ucp_worker_h CreatetUcpWorker(ucp_context_h ucp_context) {
-  ucp_worker_params_t worker_params;
-  std::memset(&worker_params, 0, sizeof(worker_params));
-  worker_params.field_mask = UCP_WORKER_PARAM_FIELD_THREAD_MODE;
-  worker_params.thread_mode = UCS_THREAD_MODE_MULTI;  // UCS_THREAD_MODE_SINGLE;
-
-  ucp_worker_h ucp_worker;
-  ucs_status_t status =
-      ucp_worker_create(ucp_context, &worker_params, &ucp_worker);
-  CheckError(status != UCS_OK, "ucp_worker_create", [&ucp_context]() {
-    ucp_cleanup(ucp_context);
-  });
-
-  return ucp_worker;
-}
-
-UcpWorkerAddress GetUcpWorkerAddress(ucp_worker_h ucp_worker) {
-  UcpWorkerAddress ucpWorkerAddress;
-
-  ucs_status_t status = ucp_worker_get_address(
-      ucp_worker, &ucpWorkerAddress.address, &ucpWorkerAddress.length);
-  CheckError(status != UCS_OK,
-             "ucp_worker_get_address",
-             [&ucp_worker /*, &ucp_context*/]() {
-               ucp_worker_destroy(ucp_worker);
-               // ucp_cleanup(ucp_context);
-             });
-
-  return ucpWorkerAddress;
-}
-
-static class ErrorHandling {
-public:
-  ucp_err_handling_mode_t ucp_err_mode;
-  int failure;
-} err_handling_opt;
-
-static void failure_handler(void *arg, ucp_ep_h, ucs_status_t status) {
-  ucs_status_t *arg_status = static_cast<ucs_status_t *>(arg);
-  std::cout << '[' << std::hex << std::this_thread::get_id()
-            << "] failure handler called with status " << status << std::endl;
-  *arg_status = status;
-}
-
-ucp_ep_h CreateUcpEp(ucp_worker_h ucp_worker,
-                     const UcpWorkerAddress &ucpWorkerAddress) {
-  static ucs_status_t current_status = UCS_OK;
-  ucp_ep_params_t ep_params;
-  ep_params.field_mask =
-      UCP_EP_PARAM_FIELD_REMOTE_ADDRESS | UCP_EP_PARAM_FIELD_ERR_HANDLING_MODE |
-      UCP_EP_PARAM_FIELD_ERR_HANDLER | UCP_EP_PARAM_FIELD_USER_DATA;
-  ep_params.address = ucpWorkerAddress.address;
-  ep_params.err_mode = err_handling_opt.ucp_err_mode;
-  ep_params.err_handler.cb = failure_handler;
-  ep_params.err_handler.arg = NULL;
-  ep_params.user_data = &current_status;
-
-  ucp_ep_h ucp_ep;
-  ucs_status_t status = ucp_ep_create(ucp_worker, &ep_params, &ucp_ep);
-  CheckError(status != UCS_OK, "ucp_ep_create");
-
-  return ucp_ep;
-}
-
-
-
-std::mutex initialize_lock;
-bool initialized = false;
 
 //=============================================================================
 
@@ -556,6 +196,9 @@ void printLoggerHeader(const std::string pathLogger, const std::string nameLogge
     }
 }
 
+
+std::mutex initialize_lock;
+bool initialized = false;
 
 /**
 * Initializes the engine and gives us shared pointers to both our transport out cache
@@ -818,13 +461,13 @@ std::pair<std::pair<std::shared_ptr<CacheMachine>,std::shared_ptr<CacheMachine> 
 		if(protocol == comm::blazing_protocol::ucx){
 			ucp_context = reinterpret_cast<ucp_context_h>(workers_ucp_info[0].context_handle);
 
-			self_worker = CreatetUcpWorker(ucp_context);
+			self_worker = ral::communication::CreatetUcpWorker(ucp_context);
 
-			UcpWorkerAddress ucpWorkerAddress = GetUcpWorkerAddress(self_worker);
+			ral::communication::UcpWorkerAddress ucpWorkerAddress = ral::communication::GetUcpWorkerAddress(self_worker);
 
-			std::map<std::string, UcpWorkerAddress> peer_addresses_map;
+			std::map<std::string, ral::communication::UcpWorkerAddress> peer_addresses_map;
 			auto th = std::thread([ralCommunicationPort, total_peers=workers_ucp_info.size(), &peer_addresses_map, worker_id, workers_ucp_info](){
-				AddressExchangerForSender exchanger(ralCommunicationPort);
+				ral::communication::AddressExchangerForSender exchanger(ralCommunicationPort);
 				for (size_t i = 0; i < total_peers; i++){
 					if(workers_ucp_info[i].worker_id == worker_id){
 						continue;
@@ -835,26 +478,26 @@ std::pair<std::pair<std::shared_ptr<CacheMachine>,std::shared_ptr<CacheMachine> 
 						// Receive worker_id size
 						size_t worker_id_buff_size;
 						ret = recv(exchanger.fd(), &worker_id_buff_size, sizeof(size_t), MSG_WAITALL);
-						CheckError(static_cast<size_t>(ret) != sizeof(size_t), "recv worker_id_buff_size");
+						ral::communication::CheckError(static_cast<size_t>(ret) != sizeof(size_t), "recv worker_id_buff_size");
 
 						// Receive worker_id
 						std::string worker_id(worker_id_buff_size, '\0');
 						ret = recv(exchanger.fd(), &worker_id[0], worker_id.size(), MSG_WAITALL);
-						CheckError(static_cast<size_t>(ret) != worker_id.size(), "recv worker_id");
+						ral::communication::CheckError(static_cast<size_t>(ret) != worker_id.size(), "recv worker_id");
 
 						// Receive ucp_worker_address size
 						size_t ucp_worker_address_size;
 						ret = recv(exchanger.fd(), &ucp_worker_address_size, sizeof(size_t), MSG_WAITALL);
-						CheckError(static_cast<size_t>(ret) != sizeof(size_t), "recv ucp_worker_address_size");
+						ral::communication::CheckError(static_cast<size_t>(ret) != sizeof(size_t), "recv ucp_worker_address_size");
 
 						// Receive ucp_worker_address
 						std::uint8_t *data = new std::uint8_t[ucp_worker_address_size];
-						UcpWorkerAddress peerUcpWorkerAddress{
+						ral::communication::UcpWorkerAddress peerUcpWorkerAddress{
 								reinterpret_cast<ucp_address_t *>(data),
 								ucp_worker_address_size};
 
 						ret = recv(exchanger.fd(), peerUcpWorkerAddress.address, ucp_worker_address_size, MSG_WAITALL);
-						CheckError(static_cast<size_t>(ret) != ucp_worker_address_size, "recv ucp_worker_address");
+						ral::communication::CheckError(static_cast<size_t>(ret) != ucp_worker_address_size, "recv ucp_worker_address");
 
 						peer_addresses_map.emplace(worker_id, peerUcpWorkerAddress);
 
@@ -868,25 +511,25 @@ std::pair<std::pair<std::shared_ptr<CacheMachine>,std::shared_ptr<CacheMachine> 
 				if(worker_info.worker_id == worker_id){
 					continue;
 				}
-				AddressExchangerForReceiver exchanger(worker_info.port, worker_info.ip.c_str());
+				ral::communication::AddressExchangerForReceiver exchanger(worker_info.port, worker_info.ip.c_str());
 				int ret;
 
 				// Send worker_id size
 				size_t worker_id_buff_size = worker_id.size();
 				ret = send(exchanger.fd(), &worker_id_buff_size, sizeof(size_t), 0);
-				CheckError(static_cast<size_t>(ret) != sizeof(size_t), "send worker_id_buff_size");
+				ral::communication::CheckError(static_cast<size_t>(ret) != sizeof(size_t), "send worker_id_buff_size");
 
 				// Send worker_id
 				ret = send(exchanger.fd(), worker_id.data(), worker_id.size(), 0);
-				CheckError(static_cast<size_t>(ret) != worker_id.size(), "send worker_id");
+				ral::communication::CheckError(static_cast<size_t>(ret) != worker_id.size(), "send worker_id");
 
 				// Send ucp_worker_address size
 				ret = send(exchanger.fd(), &ucpWorkerAddress.length, sizeof(size_t), 0);
-				CheckError(static_cast<size_t>(ret) != sizeof(size_t), "send ucp_worker_address_size");
+				ral::communication::CheckError(static_cast<size_t>(ret) != sizeof(size_t), "send ucp_worker_address_size");
 
 				// Send ucp_worker_address
 				ret = send(exchanger.fd(), ucpWorkerAddress.address, ucpWorkerAddress.length, 0);
-				CheckError(static_cast<size_t>(ret) != ucpWorkerAddress.length, "send ucp_worker_address");
+				ral::communication::CheckError(static_cast<size_t>(ret) != ucpWorkerAddress.length, "send ucp_worker_address");
 			}
 
 			th.join();
@@ -895,8 +538,8 @@ std::pair<std::pair<std::shared_ptr<CacheMachine>,std::shared_ptr<CacheMachine> 
 				if(worker_info.worker_id == worker_id){
 					continue;
 				}
-				UcpWorkerAddress peerUcpWorkerAddress = peer_addresses_map[worker_info.worker_id];
-				ucp_ep_h ucp_ep = CreateUcpEp(self_worker, peerUcpWorkerAddress);
+				ral::communication::UcpWorkerAddress peerUcpWorkerAddress = peer_addresses_map[worker_info.worker_id];
+				ucp_ep_h ucp_ep = ral::communication::CreateUcpEp(self_worker, peerUcpWorkerAddress);
 
 				// std::cout << '[' << std::hex << std::this_thread::get_id()
 				// 					<< "] local: " << std::hex
--- engine/tests/communication/send_and_receive_test.cpp
+++ engine/tests/communication/send_and_receive_test.cpp
@@ -25,7 +25,9 @@ struct SendAndReceiveTest : public BlazingUnitTest {
 		blazing_host_memory_resource::getInstance().initialize(0.5);
   }
 
-  ~SendAndReceiveTest() {}
+  ~SendAndReceiveTest() {
+    ral::memory::empty_pools();
+  }
 };
 
 
@@ -64,9 +66,28 @@ std::unique_ptr<ral::frame::BlazingTable> build_custom_table() {
 	return std::make_unique<ral::frame::BlazingTable>(ral::frame::BlazingTable(std::move(table), column_names));
 }
 
+std::unique_ptr<ral::frame::BlazingTable> build_big_table(cudf::size_type size) {
+    
+	auto num_column_1 = make_col<int32_t>(size);
+	auto num_column_2 = make_col<int64_t>(size);
+	auto num_column_3 = make_col<float>(size);
+	auto num_column_4 = make_col<double>(size);
+
+	std::vector<std::unique_ptr<cudf::column>> columns;
+	columns.push_back(std::move(num_column_1));
+	columns.push_back(std::move(num_column_2));
+	columns.push_back(std::move(num_column_3));
+	columns.push_back(std::move(num_column_4));
+
+	std::vector<std::string> column_names = {"INT64", "INT32", "FLOAT64", "FLOAT32"};
+
+	auto table = std::make_unique<cudf::table>(std::move(columns));
+	return std::make_unique<ral::frame::BlazingTable>(ral::frame::BlazingTable(std::move(table), column_names));
+}
 
 
-void setup_sender(){
+
+std::shared_ptr<ral::cache::CacheMachine> setup_sender(){
 
   uint16_t ralId = 1;
   std::string worker_id = "1";
@@ -87,6 +108,36 @@ void setup_sender(){
 			num_comm_threads, ucp_context, self_worker, ralId,protocol,require_acknowledge);
   comm::message_sender::get_instance()->run_polling();
 
+  return comm::message_sender::get_instance()->get_output_cache();
+
+}
+
+
+std::shared_ptr<ral::cache::CacheMachine> setup_receiver() {
+
+  uint16_t ralId = 0;
+  std::string worker_id = "0";
+  std::string ip = "127.0.0.1";
+  std::int32_t port = 8890;
+  std::map<std::string, comm::node> nodes_info_map;
+  nodes_info_map.emplace(worker_id, comm::node(ralId, worker_id, ip, port));
+
+  std::shared_ptr<ral::cache::CacheMachine> input_cache = std::make_shared<CacheMachine>(nullptr, "messages_in", false);
+
+  int num_comm_threads = 20;
+  comm::tcp_message_listener::initialize_message_listener(nodes_info_map,port,num_comm_threads, input_cache);
+  comm::tcp_message_listener::get_instance()->start_polling();
+
+  return comm::tcp_message_listener::get_instance()->get_input_cache(); 
+  
+}
+
+
+TEST_F(SendAndReceiveTest, SendAndReceiveTest0) {
+
+  std::shared_ptr<ral::cache::CacheMachine> output_cache = setup_sender();
+  std::shared_ptr<ral::cache::CacheMachine> input_cache = setup_receiver();
+  
   auto table = build_custom_table();
 
   ral::cache::MetadataDictionary metadata;
@@ -106,48 +157,119 @@ void setup_sender(){
   metadata.add_value(ral::cache::MESSAGE_ID, MESSAGE_ID_CONTENT);
     
   output_cache->addToCache(std::move(table),"",true,metadata,true, true);
-}
-
-
-void setup_receiver() {
-
-  uint16_t ralId = 0;
-  std::string worker_id = "0";
-  std::string ip = "127.0.0.1";
-  std::int32_t port = 8890;
-  std::map<std::string, comm::node> nodes_info_map;
-  nodes_info_map.emplace(worker_id, comm::node(ralId, worker_id, ip, port));
-
-  std::shared_ptr<ral::cache::CacheMachine> input_cache = std::make_shared<CacheMachine>(nullptr, "messages_in", false);
-
-  int num_comm_threads = 20;
-  comm::tcp_message_listener::initialize_message_listener(nodes_info_map,port,num_comm_threads, input_cache);
-  comm::tcp_message_listener::get_instance()->start_polling();
 
   auto received_table = input_cache->pullFromCache();
-  auto table = build_custom_table();
 
-  cudf::test::expect_tables_equivalent(table->view(), received_table->view());	
-  
+  auto table_for_comparison = build_custom_table();
+  cudf::test::expect_tables_equivalent(table_for_comparison->view(), received_table->view());	
+
 }
 
 
-TEST_F(SendAndReceiveTest, SendAndReceiveTest0) {
 
-  auto receiver_thread = std::thread([] {
-    setup_receiver();
-  });
 
-  std::this_thread::sleep_for(std::chrono::seconds(1));
+TEST_F(SendAndReceiveTest, SendAndReceiveALOT) {
 
-  auto sender_thread = std::thread([] {
-    setup_sender();
-  });
-  sender_thread.join();
-  receiver_thread.join();
-  ral::memory::empty_pools();
+  std::shared_ptr<ral::cache::CacheMachine> output_cache = setup_sender();
+  std::shared_ptr<ral::cache::CacheMachine> input_cache = setup_receiver();
+
+  int num_send = 20;
+  cudf::size_type big_table_size = 1000000;
+
+  std::vector<std::thread> send_threads;
+  for (int i = 0; i < num_send; i++){
+    send_threads.push_back(std::thread([output_cache, big_table_size, i]{
+      
+      auto table = build_big_table(big_table_size);
+
+      ral::cache::MetadataDictionary metadata;
+      metadata.add_value(ral::cache::RAL_ID_METADATA_LABEL,1);
+      metadata.add_value(ral::cache::KERNEL_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::QUERY_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::ADD_TO_SPECIFIC_CACHE_METADATA_LABEL, "false");
+      metadata.add_value(ral::cache::CACHE_ID_METADATA_LABEL, "");
+      metadata.add_value(ral::cache::SENDER_WORKER_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::WORKER_IDS_METADATA_LABEL, "1");
+      metadata.add_value(ral::cache::UNIQUE_MESSAGE_ID, std::to_string(1));
+
+      const std::string MESSAGE_ID_CONTENT = metadata.get_values()[ral::cache::QUERY_ID_METADATA_LABEL] + "_" +
+                                              metadata.get_values()[ral::cache::KERNEL_ID_METADATA_LABEL] + "_" +
+                                              metadata.get_values()[ral::cache::SENDER_WORKER_ID_METADATA_LABEL];
+
+      metadata.add_value(ral::cache::MESSAGE_ID, MESSAGE_ID_CONTENT);
+        
+      output_cache->addToCache(std::move(table),"",true,metadata,true, true);
+      std::cout<<"output_cache->addToCache "<<i<<std::endl;
+    }));
+  }
+
+  std::vector<std::thread> receive_threads;
+  auto table_for_comparison = build_big_table(big_table_size);
+  for (int i = 0; i < num_send; i++){
+    receive_threads.push_back(std::thread([input_cache, &table_for_comparison,i]{
+
+      auto received_table = input_cache->pullFromCache();
+      std::cout<<"input_cache->pullFromCache() "<<i<<std::endl;
+
   
+      cudf::test::expect_tables_equivalent(table_for_comparison->view(), received_table->view());	
+      }));
+  }
+
+  for (int i = 0; i < num_send; i++){ 
+    send_threads[i].join();
+  }
+  for (int i = 0; i < num_send; i++){ 
+    receive_threads[i].join();
+  }
+
+  std::cout<<"FIRST BATCH COMPLETE"<<std::endl;
 
 
+  send_threads.resize(0);
+  for (int i = 0; i < num_send; i++){
+    send_threads.push_back(std::thread([output_cache, big_table_size]{
+        
+      auto table = build_big_table(big_table_size);
+
+      ral::cache::MetadataDictionary metadata;
+      metadata.add_value(ral::cache::RAL_ID_METADATA_LABEL,1);
+      metadata.add_value(ral::cache::KERNEL_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::QUERY_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::ADD_TO_SPECIFIC_CACHE_METADATA_LABEL, "false");
+      metadata.add_value(ral::cache::CACHE_ID_METADATA_LABEL, "");
+      metadata.add_value(ral::cache::SENDER_WORKER_ID_METADATA_LABEL, 0);
+      metadata.add_value(ral::cache::WORKER_IDS_METADATA_LABEL, "1");
+      metadata.add_value(ral::cache::UNIQUE_MESSAGE_ID, std::to_string(1));
+
+      const std::string MESSAGE_ID_CONTENT = metadata.get_values()[ral::cache::QUERY_ID_METADATA_LABEL] + "_" +
+                                              metadata.get_values()[ral::cache::KERNEL_ID_METADATA_LABEL] + "_" +
+                                              metadata.get_values()[ral::cache::SENDER_WORKER_ID_METADATA_LABEL];
+
+      metadata.add_value(ral::cache::MESSAGE_ID, MESSAGE_ID_CONTENT);
+        
+      output_cache->addToCache(std::move(table),"",true,metadata,true, true);
+    }));
+  }
+
+  receive_threads.resize(0);
+  for (int i = 0; i < num_send; i++){
+    receive_threads.push_back(std::thread([input_cache, &table_for_comparison]{
+
+      auto received_table = input_cache->pullFromCache();
+  
+      cudf::test::expect_tables_equivalent(table_for_comparison->view(), received_table->view());	
+      }));
+  }
+
+  for (int i = 0; i < num_send; i++){ 
+    send_threads[i].join();
+  }
+  for (int i = 0; i < num_send; i++){ 
+    receive_threads[i].join();
+  }
+
+  std::cout<<"SECOND BATCH COMPLETE"<<std::endl;
+  
 }
 
