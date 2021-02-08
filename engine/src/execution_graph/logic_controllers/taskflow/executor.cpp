88fb45d2bc553483b9492bdd1d1e36d272bfa959
Rommel <rommel@blazingdb.com>
2021-02-08 12:01:00 -0500

    Removing couts

--- engine/src/execution_graph/logic_controllers/taskflow/executor.cpp
+++ engine/src/execution_graph/logic_controllers/taskflow/executor.cpp
@@ -86,7 +86,6 @@ void task::run(cudaStream_t stream, executor * executor){
                     input_gpu.push_back(std::move(input->decache()));
             }
     }catch(rmm::bad_alloc e){
-        std::cout<<"ERROR of type rmm::bad_alloc in task::run"<<std::endl;
         int i = 0;
         for(auto & input : inputs){
             if (i < last_input_decached && (input->get_type() == ral::cache::CacheDataType::GPU || input->get_type() == ral::cache::CacheDataType::GPU_METADATA)){
@@ -99,7 +98,7 @@ void task::run(cudaStream_t stream, executor * executor){
         std::shared_ptr<spdlog::logger> logger = spdlog::get("batch_logger");
         if (logger){
             logger->error("|||{info}|||||",
-                    "info"_a="ERROR of type rmm::bad_alloc in task::run. What: {}"_format(e.what()));            
+                    "info"_a="ERROR of type rmm::bad_alloc in task::run. What: {}"_format(e.what()));
         }
 
         this->attempts++;
@@ -107,11 +106,9 @@ void task::run(cudaStream_t stream, executor * executor){
             executor->add_task(std::move(inputs), output, kernel, attempts, task_id, args);
             return;
         }else{
-            std::cout<<"ERROR of type rmm::bad_alloc in task::run. Ran out of attempts. Throwing"<<std::endl;
             throw;
         }
     }catch(std::exception & e){
-        std::cout<<"ERROR of type std::exception in task::run"<<std::endl;
         std::shared_ptr<spdlog::logger> logger = spdlog::get("batch_logger");
         if (logger){
             logger->error("|||{info}|||||",
@@ -132,7 +129,7 @@ void task::run(cudaStream_t stream, executor * executor){
                 if  (input->get_type() == ral::cache::CacheDataType::GPU || input->get_type() == ral::cache::CacheDataType::GPU_METADATA){
                     //this was a gpu cachedata so now its not valid
                     if(task_result.inputs.size() > 0 && i <= task_result.inputs.size() && task_result.inputs[i] != nullptr && task_result.inputs[i]->is_valid()){ 
-                        static_cast<ral::cache::GPUCacheData *>(input.get())->set_data(std::move(task_result.inputs[i]));                        
+                        static_cast<ral::cache::GPUCacheData *>(input.get())->set_data(std::move(task_result.inputs[i]));
                     }else{
                         //the input was lost and it was a gpu dataframe which is not recoverable
                         throw rmm::bad_alloc(task_result.what.c_str());
@@ -147,11 +144,9 @@ void task::run(cudaStream_t stream, executor * executor){
         if(this->attempts < this->attempts_limit){
             executor->add_task(std::move(inputs), output, kernel, attempts, task_id, args);
         }else{
-            std::cout<<"ERROR in task::run. Ran out of attempts. Throwing"<<std::endl;
             throw rmm::bad_alloc("Ran out of memory processing");
         }
     }else{
-        std::cout<<"ERROR in task::run. ELSE?"<<std::endl;
         throw std::runtime_error(task_result.what.c_str());
     }
 }
@@ -219,7 +214,6 @@ void executor::execute(){
         try {
             f.get();
         } catch(...) {
-            std::cout<<"caught in execute()"<<std::endl;
             std::unique_lock<std::mutex> lock(exception_holder_mutex);
             exception_holder.push(std::current_exception());
             cur_task->fail();
