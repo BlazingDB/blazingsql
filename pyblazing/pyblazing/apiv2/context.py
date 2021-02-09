8e0f25e46eb6312f9b672c90d38b45a51088f862
Fernando Rodriguez <fernando@blazingdb.com>
2021-02-09 12:18:14 -0500

    apply format to context.py

--- pyblazing/pyblazing/apiv2/context.py
+++ pyblazing/pyblazing/apiv2/context.py
@@ -3154,7 +3154,14 @@ class BlazingContext(object):
                     ["int32", "int32", "int64", "str", "str"],
                 ),
                 "bsql_kernels": (
-                    ["ral_id", "query_id", "kernel_id", "is_kernel", "kernel_type", "description"],
+                    [
+                        "ral_id",
+                        "query_id",
+                        "kernel_id",
+                        "is_kernel",
+                        "kernel_type",
+                        "description",
+                    ],
                     ["int32", "int32", "int64", "int16", "str", "str"],
                 ),
                 "bsql_kernels_edges": (
