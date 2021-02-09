95bd90cc5e6c03d50b33d6da4a45eb693baa0c44
Fernando Rodriguez <fernando@blazingdb.com>
2021-02-09 09:49:05 -0500

    update logging test

--- tests/BlazingSQLTest/EndToEndTests/loggingTest.py
+++ tests/BlazingSQLTest/EndToEndTests/loggingTest.py
@@ -94,12 +94,10 @@ def main(dask_client, dir_data_file, bc, nRals):
         )
         queryId = "TEST_07"
         query = """SELECT 
-                    ral_id, query_id, source, sink, num_rows, num_bytes, 
+                    ral_id, query_id, source, cache_id, num_rows, num_bytes, 
                     event_type, timestamp_begin, timestamp_end 
                 FROM 
                     bsql_cache_events 
-                WHERE 
-                    event_type = 'addCache' 
                 ORDER BY 
                     timestamp_begin DESC"""
         runTest.run_query_log(
