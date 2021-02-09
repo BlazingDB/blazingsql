d08de1316de04602d6666f5f3038cc6ab3f5f232
Rommel <rommel@blazingdb.com>
2021-02-09 13:53:54 -0500

    Removed duplicated e2e test (same as tpchQueries 8)

--- tests/BlazingSQLTest/EndToEndTests/caseTest.py
+++ tests/BlazingSQLTest/EndToEndTests/caseTest.py
@@ -86,27 +86,6 @@ def main(dask_client, drill, spark, dir_data_lc, bc, nRals):
             #           worder, '', acceptable_difference, use_percentage,
             #           fileSchemaType)
 
-            queryId = "TEST_03"
-            query = """SELECT O_YEAR, SUM(CASE WHEN NATION = 'BRAZIL' THEN
-                    VOLUME ELSE 0 END)/SUM(VOLUME) AS MKT_SHARE
-                    FROM (SELECT datepart(yy,O_ORDERDATE) AS O_YEAR,
-                        L_EXTENDEDPRICE*(1-L_DISCOUNT) AS VOLUME,
-                        N2.N_NAME AS NATION
-                    FROM PART, SUPPLIER, LINEITEM, ORDERS, CUSTOMER,
-                    NATION N1, NATION N2, REGION
-                    WHERE P_PARTKEY = L_PARTKEY AND S_SUPPKEY = L_SUPPKEY
-                    AND L_ORDERKEY = O_ORDERKEY
-                    AND O_CUSTKEY = C_CUSTKEY AND C_NATIONKEY = N1.N_NATIONKEY
-                    AND N1.N_REGIONKEY = R_REGIONKEY AND R_NAME = 'AMERICA'
-                    AND S_NATIONKEY = N2.N_NATIONKEY
-                    AND O_ORDERDATE > '1995-01-01'
-                    AND P_TYPE= 'ECONOMY ANODIZED STEEL') AS ALL_NATIONS
-                    GROUP BY O_YEAR
-                    ORDER BY O_YEAR"""
-            # Doesn't work syntactically
-            # runTest.run_query(bc, drill, query, queryId, queryType, worder,
-            #  '', acceptable_difference, use_percentage, fileSchemaType)
-
             queryId = "TEST_04"
             query = """select r_name, (case when r_name = 'AFRICA' then
                     r_name  else 'AFROBEAT' end) as n_rname
