3ec58dd639d237654d9ff7184e2b697d189a6337
Fernando Rodriguez <fernando@blazingdb.com>
2021-02-22 20:01:17 -0500

    add function for create partitions

--- tests/BlazingSQLTest/EndToEndTests/hiveFileTest.py
+++ tests/BlazingSQLTest/EndToEndTests/hiveFileTest.py
@@ -4,7 +4,7 @@ from Configuration import Settings as Settings
 from DataBase import createSchema as cs
 from pynvml import nvmlInit
 from Runner import runTest
-from Utils import Execution, gpuMemory, init_context, skip_test
+from Utils import Execution, gpuMemory, init_context, skip_test, utilityHive
 
 queryType = "Hive File"
 
@@ -58,6 +58,121 @@ def main(dask_client, spark, dir_data_file, bc, nRals):
 
     gpuMemory.log_memory_usage(queryType, start_mem, end_mem)
 
+def createPartitions(fileSchemaType, dir_data_file):
+    dir_data = dir_data_file + "/tpch"
+
+    ext = cs.get_extension(fileSchemaType)
+
+    # orders table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "orders", ext),
+                                           table_name="orders",
+                                           partitions={
+                                               'o_orderpriority': ['1-URGENT', '2-HIGH', '3-MEDIUM', '4-NOT SPECIFIED',
+                                                                   '5-LOW'],
+                                               'o_orderstatus': ['F', 'O', 'P']},
+                                           partitions_schema=[('o_orderpriority', 'str'),
+                                                              ('o_orderstatus', 'str')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/orders/',
+                                           num_files=4)
+
+    # customer table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "customer", ext),
+                                           table_name="customer",
+                                           partitions={
+                                               'c_nationkey': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24],
+                                               'c_mktsegment': ['AUTOMOBILE', 'BUILDING', 'FURNITURE', 'HOUSEHOLD', 'MACHINERY']},
+                                         partitions_schema=[('c_nationkey', 'int32'),
+                                                            ('c_mktsegment', 'str')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/customer/',
+                                           num_files=4)
+
+    # lineitem table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "lineitem", ext),
+                                           table_name="lineitem",
+                                           partitions={
+                                                'l_shipmode':['AIR','FOB','MAIL','RAIL','REG','AIR','SHIP','TRUCK'],
+                                                'l_linestatus':['F','O'],
+                                                'l_returnflag':['A','N','R']
+                                           },
+                                         partitions_schema=[('l_shipmode', 'str'),
+                                                            ('l_linestatus', 'str'),
+                                                            ('l_returnflag', 'str')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/lineitem/',
+                                           num_files=4)
+
+    # nation table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "nation", ext),
+                                           table_name="nation",
+                                           partitions={
+                                               'n_regionkey': [0,1,2,3,4]},
+                                         partitions_schema=[('n_regionkey', 'int32')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/nation/',
+                                           num_files=4)
+
+    # part table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "part", ext),
+                                           table_name="part",
+                                           partitions={
+                                               'p_container': ['JUMBO BAG', 'JUMBO BOX', 'JUMBO CAN', 'JUMBO CASE',
+                                                               'JUMBO DRUM', 'JUMBO JAR', 'JUMBO PACK', 'JUMBO PKG',
+                                                               'LG BAG', 'LG BOX', 'LG CAN', 'LG CASE', 'LG DRUM',
+                                                               'LG JAR', 'LG PACK', 'LG PKG', 'MED BAG', 'MED BOX',
+                                                               'MED CAN', 'MED CASE', 'MED DRUM', 'MED JAR', 'MED PACK',
+                                                               'MED PKG', 'SM BAG', 'SM BOX', 'SM CAN', 'SM CASE',
+                                                               'SM DRUM', 'SM JAR', 'SM PACK', 'SM PKG', 'WRAP BAG',
+                                                               'WRAP BOX', 'WRAP CAN', 'WRAP CASE', 'WRAP DRUM',
+                                                               'WRAP JAR', 'WRAP PACK', 'WRAP PKG']},
+                                         partitions_schema=[('p_container', 'str')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/part/',
+                                           num_files=4)
+
+    # supplier table
+    utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "supplier", ext),
+                                           table_name="supplier",
+                                           partitions={
+                                               's_nationkey': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]},
+                                         partitions_schema=[('s_nationkey', 'int32')],
+                                           output='/tmp/BlazingSQL/partitions/' + ext + '/supplier/',
+                                           num_files=4)
+
+    # order table with datetime
+    # utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "orders", ext),
+    #                                      table_name="orders",
+    #                                      partitions={
+    #                                          'o_orderdate': [694224000000, 694310400000, 694396800000, 694483200000, 694569600000,
+    #                                                          694656000000, 694742400000, 694828800000, 694915200000, 695001600000]},
+    #                                      partitions_schema=[('o_orderdate', 'timestamp')],
+    #                                      output='/tmp/BlazingSQL/partitions/' + ext + '/ordersDatetime/',
+    #                                      num_files=4)
+
+    # '1992-01-01 00:00:00', '1992-01-02 00:00:00',
+    # '1992-01-03 00:00:00', '1992-01-04 00:00:00',
+    # '1992-01-05 00:00:00', '1992-01-06 00:00:00',
+    # '1992-01-07 00:00:00', '1992-01-08 00:00:00',
+    # '1992-01-09 00:00:00', '1992-01-10 00:00:00',
+    # '1992-01-11 00:00:00', '1992-01-12 00:00:00',
+    # '1992-01-13 00:00:00', '1992-01-14 00:00:00',
+    # '1992-01-15 00:00:00', '1992-01-16 00:00:00',
+    # '1992-01-17 00:00:00', '1992-01-18 00:00:00',
+    # '1992-01-19 00:00:00', '1992-01-20 00:00:00',
+    # '1992-01-21 00:00:00', '1992-01-22 00:00:00',
+    # '1992-01-23 00:00:00', '1992-01-24 00:00:00',
+    # '1992-01-25 00:00:00', '1992-01-26 00:00:00',
+    # '1992-01-27 00:00:00', '1992-01-28 00:00:00',
+    # '1992-01-29 00:00:00', '1992-01-30 00:00:00',
+    # '1992-01-31 00:00:00'
+# 694224000, 694310400, 694396800, 694483200, 694569600, 694656000, 694742400, 694828800, 694915200, 695001600
+    # supplier table with Boolean
+    # utilityHive.test_hive_partition_data(input=("%s/%s_[0-9]*.%s") % (dir_data, "supplier", ext),
+    #                                      table_name="supplier",
+    #                                      partitions={
+    #                                          's_nationkey': [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+    #                                                          17, 18, 19, 20, 21, 22, 23, 24]},
+    #                                      partitions_schema=[('s_nationkey', 'int32')],
+    #                                      output='/tmp/BlazingSQL/partitions/' + ext + '/supplier/',
+    #                                      num_files=4)
+
+    return '/tmp/BlazingSQL/partitions/' + ext + '/'
 
 if __name__ == "__main__":
 
