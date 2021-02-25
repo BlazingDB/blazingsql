77ea33f34de2e257c5972e8a86fc27abb8153cb4
William Malpica <william@blazingdb.com>
2021-02-25 14:24:52 -0600

    forgot to add the actual unit test file

---
+++ engine/tests/kernel_tests/window_overlap_kernel_test.cpp
@@ -0,0 +1,259 @@
+#include <spdlog/spdlog.h>
+#include "tests/utilities/BlazingUnitTest.h"
+
+#include <chrono>
+#include <thread>
+//#include <gtest/gtest.h>
+
+#include "cudf_test/column_wrapper.hpp"
+#include "cudf_test/type_lists.hpp"	 // cudf::test::NumericTypes
+#include <cudf_test/table_utilities.hpp>
+
+#include "execution_graph/Context.h"
+#include "execution_graph/logic_controllers/taskflow/kernel.h"
+#include "execution_graph/logic_controllers/taskflow/graph.h"
+#include "execution_graph/logic_controllers/taskflow/port.h"
+#include "execution_graph/logic_controllers/BatchWindowFunctionProcessing.h"
+#include "execution_graph/logic_controllers/taskflow/executor.h"
+
+using blazingdb::transport::Node;
+using ral::cache::kstatus;
+using ral::cache::CacheMachine;
+using ral::cache::CacheData;
+using ral::frame::BlazingTable;
+using ral::cache::kernel;
+using Context = blazingdb::manager::Context;
+
+
+struct WindowOverlapTest : public ::testing::Test {
+	virtual void SetUp() override {
+		BlazingRMMInitialize();
+		float host_memory_quota=0.75; //default value
+		blazing_host_memory_resource::getInstance().initialize(host_memory_quota);
+		ral::memory::set_allocation_pools(4000000, 10,
+										4000000, 10, false,nullptr);
+		int executor_threads = 10;
+		ral::execution::executor::init_executor(executor_threads, 0.8);
+	}
+
+	virtual void TearDown() override {
+		ral::memory::empty_pools();
+		BlazingRMMFinalize();
+	}
+};
+
+
+
+// Just creates a Context
+std::shared_ptr<Context> make_context(int num_nodes) {
+	std::vector<Node> nodes(num_nodes);
+	for (int i = 0; i < num_nodes; i++){
+		nodes[i] = Node(std::to_string(i));
+	}
+	Node master_node("0");
+	std::string logicalPlan;
+	std::map<std::string, std::string> config_options;
+	std::shared_ptr<Context> context = std::make_shared<Context>(0, nodes, master_node, logicalPlan, config_options);
+
+	return context;
+}
+
+// Creates a OverlapAccumulatorKernel using a valid `project_plan`
+std::shared_ptr<kernel> make_overlap_kernel(std::string project_plan, std::shared_ptr<Context> context) {
+	std::size_t kernel_id = 1;
+	std::shared_ptr<ral::cache::graph> graph = std::make_shared<ral::cache::graph>();
+	std::shared_ptr<kernel> overlap_kernel = std::make_shared<ral::batch::OverlapAccumulatorKernel>(kernel_id, project_plan, context, graph);
+
+	return overlap_kernel;
+}
+
+// Creates two CacheMachines and register them with the `project_kernel`
+std::tuple<std::shared_ptr<CacheMachine>, std::shared_ptr<CacheMachine>, std::shared_ptr<CacheMachine>, std::shared_ptr<CacheMachine>> register_kernel_with_cache_machines(
+	std::shared_ptr<kernel> overlap_kernel,
+	std::shared_ptr<Context> context) {
+	std::shared_ptr<CacheMachine>  batchesCacheMachine = std::make_shared<CacheMachine>(context, "batches");
+	std::shared_ptr<CacheMachine>  precedingCacheMachine = std::make_shared<CacheMachine>(context, "preceding_overlaps");
+	std::shared_ptr<CacheMachine>  followingCacheMachine = std::make_shared<CacheMachine>(context, "following_overlaps");
+	std::shared_ptr<CacheMachine> outputCacheMachine = std::make_shared<CacheMachine>(context, "1");
+	overlap_kernel->input_.register_cache("batches", batchesCacheMachine);
+	overlap_kernel->input_.register_cache("preceding_overlaps", precedingCacheMachine);
+	overlap_kernel->input_.register_cache("following_overlaps", followingCacheMachine);
+	overlap_kernel->output_.register_cache("1", outputCacheMachine);
+
+	return std::make_tuple(batchesCacheMachine, precedingCacheMachine, followingCacheMachine, outputCacheMachine);	
+}
+
+// Feeds an input cache with time delays
+void add_data_to_cache_with_delay(
+	std::shared_ptr<CacheMachine> cache_machine,
+	std::vector<std::unique_ptr<BlazingTable>> batches,
+	std::vector<int> delays_in_ms)
+{
+	int total_batches = batches.size();
+	int total_delays = delays_in_ms.size();
+
+	EXPECT_EQ(total_delays, total_batches);
+
+	for (int i = 0; i < total_batches; ++i) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(delays_in_ms[i]));
+		cache_machine->addToCache(std::move(batches[i]));
+	}
+
+	// default last delay
+	std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+	cache_machine->finish();
+}
+
+std::tuple<std::vector<std::unique_ptr<CacheData>>, std::vector<std::unique_ptr<CacheData>>, std::vector<std::unique_ptr<CacheData>>> break_up_full_data(
+		CudfTableView full_data_cudf_view, int preceding_value, int following_value, std::vector<cudf::size_type> batch_sizes, std::vector<std::string> names){
+	
+	std::vector<cudf::size_type> split_indexes = batch_sizes;
+	std::partial_sum(split_indexes.begin(), split_indexes.end(), split_indexes.begin());
+	split_indexes.erase(split_indexes.begin() + split_indexes.size() - 1);
+
+	auto split_views = cudf::split(full_data_cudf_view, split_indexes);
+	
+	std::vector<std::unique_ptr<CacheData>> preceding_overlaps, batches, following_overlaps;
+	std::vector<std::unique_ptr<BlazingTable>> batch_tables;
+	for (int i = 0; i < split_views.size(); i++){
+		auto cudf_table = std::make_unique<CudfTable>(split_views[i]);
+		auto blz_table = std::make_unique<BlazingTable>(std::move(cudf_table), names);
+		batch_tables.push_back(std::move(blz_table));
+	}
+	for (int i = 0; i < split_views.size() - 1; i++){
+		std::vector<cudf::size_type> presceding_split_index = {preceding_value > batch_tables[i]->num_rows() ? preceding_value : batch_tables[i]->num_rows()};
+		auto preceding_split_views = cudf::split(batch_tables[i]->view(), presceding_split_index);
+		auto cudf_table = std::make_unique<CudfTable>(preceding_split_views[0]);
+		auto blz_table = std::make_unique<BlazingTable>(std::move(cudf_table), names);
+		std::string overlap_status = preceding_value > batch_tables[i]->num_rows() ? ral::batch::INCOMPLETE_OVERLAP_STATUS : ral::batch::DONE_OVERLAP_STATUS;
+		ral::cache::MetadataDictionary metadata;
+		metadata.add_value(ral::cache::OVERLAP_STATUS, overlap_status);
+		preceding_overlaps.push_back(std::make_unique<ral::cache::GPUCacheData>(std::move(blz_table),metadata));
+	}
+	for (int i = 1; i < split_views.size(); i++){
+		std::vector<cudf::size_type> following_split_index = {following_value > batch_tables[i]->num_rows() ? 0 : batch_tables[i]->num_rows() - following_value};
+		auto following_split_views = cudf::split(batch_tables[i]->view(), following_split_index);
+		auto cudf_table = std::make_unique<CudfTable>(following_split_views[1]);
+		auto blz_table = std::make_unique<BlazingTable>(std::move(cudf_table), names);
+		std::string overlap_status = following_value > batch_tables[i]->num_rows() ? ral::batch::INCOMPLETE_OVERLAP_STATUS : ral::batch::DONE_OVERLAP_STATUS;
+		ral::cache::MetadataDictionary metadata;
+		metadata.add_value(ral::cache::OVERLAP_STATUS, overlap_status);
+		following_overlaps.push_back(std::make_unique<ral::cache::GPUCacheData>(std::move(blz_table),metadata));
+	}
+	for (int i = 0; i < split_views.size(); i++){
+		batches.push_back(std::make_unique<ral::cache::GPUCacheData>(std::move(batch_tables[i])));
+	}
+
+	return std::make_tuple(std::move(preceding_overlaps), std::move(batches), std::move(following_overlaps));
+}
+
+std::vector<std::unique_ptr<BlazingTable>> make_expected_output(
+		CudfTableView full_data_cudf_view, int preceding_value, int following_value, std::vector<cudf::size_type> batch_sizes, std::vector<std::string> names){
+	
+	std::vector<cudf::size_type> split_indexes = batch_sizes;
+	std::partial_sum(split_indexes.begin(), split_indexes.end(), split_indexes.begin());
+	split_indexes.erase(split_indexes.begin() + split_indexes.size() - 1);
+
+	std::vector<std::unique_ptr<BlazingTable>> out_batches;
+	for (int i = 0; i < batch_sizes.size(); i++){
+		if (i == 0){
+			std::vector<cudf::size_type> out_split_index = {split_indexes[0] + following_value};
+			auto out_split_views = cudf::split(full_data_cudf_view, out_split_index);
+			auto cudf_table = std::make_unique<CudfTable>(out_split_views[0]);
+			out_batches.push_back(std::make_unique<BlazingTable>(std::move(cudf_table), names));
+		} else if (i == batch_sizes.size() - 1){
+			std::vector<cudf::size_type> out_split_index = {full_data_cudf_view.num_rows() - batch_sizes[batch_sizes.size() - 1] - preceding_value};
+			auto out_split_views = cudf::split(full_data_cudf_view, out_split_index);
+			auto cudf_table = std::make_unique<CudfTable>(out_split_views[1]);
+			out_batches.push_back(std::make_unique<BlazingTable>(std::move(cudf_table), names));
+		} else {
+			std::vector<cudf::size_type> out_split_index = {split_indexes[i] - preceding_value, split_indexes[i] + following_value};
+			auto out_split_views = cudf::split(full_data_cudf_view, out_split_index);
+			auto cudf_table = std::make_unique<CudfTable>(out_split_views[1]);
+			out_batches.push_back(std::make_unique<BlazingTable>(std::move(cudf_table), names));
+		}		
+	}
+	return std::move(out_batches);
+}
+
+TEST_F(WindowOverlapTest, BasicSingleNode) {
+
+	size_t size = 100000;
+
+	// Define full dataset
+	auto iter0 = cudf::detail::make_counting_transform_iterator(0, [](auto i) { return int32_t(i);});
+	// auto iter1 = cudf::detail::make_counting_transform_iterator(1000, [](auto i) { return int32_t(i * 2);});
+	// auto iter2 = cudf::detail::make_counting_transform_iterator(0, [](auto i) { return int32_t(i % 5);});
+	auto valids_iter = cudf::detail::make_counting_transform_iterator(0, [](auto i) { return i%2==0? true:false; });
+	
+    cudf::test::fixed_width_column_wrapper<int32_t> col0(iter0, iter0 + size, valids_iter);
+	// cudf::test::fixed_width_column_wrapper<int32_t> col1(iter1, iter1 + size, valids_iter);
+	// cudf::test::fixed_width_column_wrapper<int32_t> col2(iter2, iter2 + size, valids_iter);
+	
+	CudfTableView full_data_cudf_view ({col0});//, col1, col2});
+
+	int preceding_value = 50;
+	int following_value = 10;
+	std::vector<cudf::size_type> batch_sizes = {5000, 50000, 20000, 15000, 10000}; // need to sum up to size
+
+	// define how its broken up into batches and overlaps
+	std::vector<std::string> names({"A"});//, "B", "C"});
+	std::vector<std::unique_ptr<CacheData>> preceding_overlaps, batches, following_overlaps;
+	std::tie(preceding_overlaps, batches, following_overlaps) = break_up_full_data(full_data_cudf_view, preceding_value, following_value, batch_sizes, names);
+
+	std::vector<std::unique_ptr<BlazingTable>> expected_out = make_expected_output(full_data_cudf_view, preceding_value, following_value, batch_sizes, names);
+
+    // create and start kernel
+	// Context
+	std::shared_ptr<Context> context = make_context(1);
+
+	auto & communicationData = ral::communication::CommunicationData::getInstance();
+	communicationData.initialize("0", "/tmp");
+
+	// overlap kernel
+	std::shared_ptr<kernel> overlap_kernel = make_overlap_kernel(
+		"LogicalProject(min_val=[MIN($0) OVER (PARTITION BY $2 ORDER BY $1 ROWS BETWEEN 50 PRECEDING AND 10 FOLLOWING)])", context);
+
+	// register cache machines with the kernel
+	std::shared_ptr<CacheMachine> batchesCacheMachine, precedingCacheMachine, followingCacheMachine, outputCacheMachine;
+	std::tie(batchesCacheMachine, precedingCacheMachine, followingCacheMachine, outputCacheMachine) = register_kernel_with_cache_machines(overlap_kernel, context);
+
+	// run function
+	std::thread run_thread = std::thread([overlap_kernel](){
+		kstatus process = overlap_kernel->run();
+		EXPECT_EQ(kstatus::proceed, process);
+	});
+
+	std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+	// add data into the CacheMachines
+	for (int i = 0; i < batch_sizes.size(); i++) {
+		batchesCacheMachine->addCacheData(std::move(batches[i]));
+		if (i != 0) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(2));
+			precedingCacheMachine->addCacheData(std::move(preceding_overlaps[i - 1]));			
+		}
+		if (i != batch_sizes.size() - 1) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(2));
+			followingCacheMachine->addCacheData(std::move(following_overlaps[i]));			
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+	}
+	batchesCacheMachine->finish();
+	precedingCacheMachine->finish();
+	followingCacheMachine->finish();	
+	
+	run_thread.join();	
+	
+
+
+	// get and validate output
+	auto batches_pulled = outputCacheMachine->pull_all_cache_data();
+	EXPECT_EQ(batches_pulled.size(), expected_out.size());
+	for (int i = 0; i < batches_pulled.size(); i++) {
+		auto table_out = batches_pulled[i]->decache();
+		cudf::test::expect_tables_equivalent(expected_out[i]->view(), table_out->view());
+	}
+}
+
