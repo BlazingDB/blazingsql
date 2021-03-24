620378d414ca4886d433bf1073b64d8550773d91
gcca <cristhian@blazingdb.com>
2021-03-24 11:12:51 -0500

    [feature/create-tables-from-rdbms] update postgresql parser

---
+++ engine/src/io/data_parser/sql/PostgreSQLParser.cpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2021 BlazingDB, Inc.
+ *     Copyright 2021 Cristhian Alberto Gonzales Castillo
+ * <cristhian@blazingdb.com>
+ */
+
+#include "PostgreSQLParser.h"
+
+namespace ral {
+namespace io {
+
+postgresql_parser::postgresql_parser() = default;
+
+postgresql_parser::~postgresql_parser() = default;
+
+std::unique_ptr<frame::BlazingTable>
+postgresql_parser::parse_batch(data_handle handle,
+                               const Schema &schema,
+                               std::vector<int> column_indices,
+                               std::vector<cudf::size_type> row_groups) {
+  return nullptr;
+}
+
+void postgresql_parser::parse_schema(ral::io::data_handle handle,
+                                     Schema &schema) {}
+
+std::unique_ptr<frame::BlazingTable>
+postgresql_parser::get_metadata(std::vector<data_handle> handles, int offset) {
+return nullptr;
+}
+
+}  // namespace io
+}  // namespace ral
