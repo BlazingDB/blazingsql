a25f3d83c2847f7c92abfa5ce57c9f0ea9b9293e
Rommel <rommel@blazingdb.com>
2021-03-25 14:32:55 -0500

    Enabling more groupby and count distinct e2e tests

--- tests/BlazingSQLTest/EndToEndTests/countDistinctTest.py
+++ tests/BlazingSQLTest/EndToEndTests/countDistinctTest.py
@@ -101,45 +101,35 @@ def main(dask_client, drill, dir_data_file, bc, nRals):
             query = """select count(distinct(o_custkey)), avg(o_totalprice),
                 (o_orderkey + o_custkey) as num from orders
                 where o_custkey < 100 group by o_custkey, o_orderkey"""
-
-            # Failed test with nulls
-            # Reported issue: https://github.com/BlazingDB/blazingsql/issues/1406
-            testsWithNulls = Settings.data["RunSettings"]["testsWithNulls"]
-            if testsWithNulls != "true":
-                runTest.run_query(
-                    bc,
-                    drill,
-                    query,
-                    queryId,
-                    queryType,
-                    worder,
-                    "",
-                    acceptable_difference,
-                    use_percentage,
-                    fileSchemaType,
-                )
+            runTest.run_query(
+                bc,
+                drill,
+                query,
+                queryId,
+                queryType,
+                worder,
+                "",
+                acceptable_difference,
+                use_percentage,
+                fileSchemaType,
+            )
 
             queryId = "TEST_05"
             query = """select count(distinct(o_custkey)), max(o_totalprice),
                     min(o_totalprice), avg(o_totalprice)
                     from orders group by o_custkey"""
-
-            # Failed test with nulls
-            # Reported issue: https://github.com/BlazingDB/blazingsql/issues/1407
-            testsWithNulls = Settings.data["RunSettings"]["testsWithNulls"]
-            if testsWithNulls != "true":
-                runTest.run_query(
-                    bc,
-                    drill,
-                    query,
-                    queryId,
-                    queryType,
-                    worder,
-                    "",
-                    0.01,
-                    use_percentage,
-                    fileSchemaType,
-                )
+            runTest.run_query(
+                bc,
+                drill,
+                query,
+                queryId,
+                queryType,
+                worder,
+                "",
+                0.01,
+                use_percentage,
+                fileSchemaType,
+            )
 
             queryId = "TEST_06"
             query = """select n_nationkey, count(distinct(
@@ -162,23 +152,18 @@ def main(dask_client, drill, dir_data_file, bc, nRals):
             query = """select count(distinct(o_orderdate)), count(distinct(o_custkey)),
                     count(distinct(o_totalprice)), sum(o_orderkey)
                     from orders group by o_custkey"""
-
-            # Failed test with nulls
-            # Reported issue: https://github.com/BlazingDB/blazingsql/issues/1408
-            testsWithNulls = Settings.data["RunSettings"]["testsWithNulls"]
-            if testsWithNulls != "true":
-                runTest.run_query(
-                    bc,
-                    drill,
-                    query,
-                    queryId,
-                    queryType,
-                    worder,
-                    "",
-                    acceptable_difference,
-                    use_percentage,
-                    fileSchemaType,
-                )
+            runTest.run_query(
+                bc,
+                drill,
+                query,
+                queryId,
+                queryType,
+                worder,
+                "",
+                acceptable_difference,
+                use_percentage,
+                fileSchemaType,
+            )
 
             queryId = 'TEST_08'
             query = """select COUNT(DISTINCT(n.n_nationkey)),
