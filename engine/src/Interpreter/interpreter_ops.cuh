1f2eba404dfbe18a3aaa35f424c44cc00f29ef9b
Rommel <rommel@blazingdb.com>
2021-03-25 18:36:31 -0500

    Removing code redundancies

--- engine/src/Interpreter/interpreter_ops.cuh
+++ engine/src/Interpreter/interpreter_ops.cuh
@@ -533,16 +533,16 @@ private:
 					setColumnValid(row_valids, output_position, false);
 				}
 			} else if(oper == operator_type::BLZ_IS_NOT_DISTINCT_FROM) {
+				bool result_value;
 				if(!left_valid && !right_valid) {
-					store_data_in_buffer(static_cast<int64_t>(true), buffer, output_position);
-					setColumnValid(row_valids, output_position, true);
+					result_value = true;
 				} else if(!left_valid || !right_valid) {
-					store_data_in_buffer(static_cast<int64_t>(false), buffer, output_position);
-					setColumnValid(row_valids, output_position, true);
+					result_value = false;
 				} else {
-					store_data_in_buffer(static_cast<int64_t>(left_value == right_value), buffer, output_position);
-					setColumnValid(row_valids, output_position, true);
+					result_value = left_value == right_value;
 				}
+				store_data_in_buffer(static_cast<int64_t>(result_value), buffer, output_position);
+				setColumnValid(row_valids, output_position, true);
 			} else if (left_valid && right_valid) {
 				if(oper == operator_type::BLZ_LOGICAL_AND) {
 					store_data_in_buffer(static_cast<int64_t>(left_value && right_value), buffer, output_position);
