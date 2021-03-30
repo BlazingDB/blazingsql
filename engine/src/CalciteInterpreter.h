36d6aa554048df6034531b8e8eb5972cd466d4a3
Fernando Rodriguez <fernando@blazingdb.com>
2021-03-30 10:05:39 -0500

    format function

--- engine/src/CalciteInterpreter.h
+++ engine/src/CalciteInterpreter.h
@@ -11,12 +11,12 @@ using blazingdb::manager::Context;
 std::string get_physical_plan(std::string logicalPlan, Context &queryContext);
 
 std::shared_ptr<ral::cache::graph> generate_graph(std::vector<ral::io::data_loader> input_loaders,
-																									std::vector<ral::io::Schema> schemas,
-																									std::vector<std::string> table_names,
-																									std::vector<std::string> table_scans,
-																									std::string logicalPlan,
-																									Context & queryContext,
-                                                                                                    const std::string &sql);
+                                                  std::vector<ral::io::Schema> schemas,
+                                                  std::vector<std::string> table_names,
+                                                  std::vector<std::string> table_scans,
+                                                  std::string logicalPlan,
+                                                  Context &queryContext,
+                                                  const std::string &sql);
 
 void start_execute_graph(std::shared_ptr<ral::cache::graph> graph);
 std::vector<std::unique_ptr<ral::frame::BlazingTable>> get_execute_graph_results(std::shared_ptr<ral::cache::graph> graph);
