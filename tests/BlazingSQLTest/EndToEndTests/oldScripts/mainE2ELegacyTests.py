716086fc1cd26e469d0ee439308e1ae968c90d60
Fernando Rodriguez <diegodfrf@gmail.com>
2021-05-06 14:21:57 -0500

    update legacy test

--- tests/BlazingSQLTest/EndToEndTests/oldScripts/mainE2ELegacyTests.py
+++ tests/BlazingSQLTest/EndToEndTests/oldScripts/mainE2ELegacyTests.py
@@ -1,33 +1,46 @@
 from Configuration import Settings
 from Configuration import ExecutionMode
 
-from EndToEndTests.oldScripts import fileSystemGSTest
-from EndToEndTests.oldScripts import fileSystemS3Test
-from EndToEndTests.oldScripts import columnBasisTest
 from EndToEndTests.oldScripts import hiveFileTest
-
-from EndToEndTests.oldScripts import configOptionsTest
-
+from EndToEndTests.oldScripts import unsignedTypeTest
+from EndToEndTests.oldScripts import columnBasisTest
+from EndToEndTests.oldScripts import dateTest
+from EndToEndTests.oldScripts import booleanTest
+from EndToEndTests.oldScripts import fileSystemHdfsTest
+# from EndToEndTests.oldScripts import mixedFileSystemTest
+# from EndToEndTests.oldScripts import tpchQueriesTest
 from EndToEndTests.oldScripts import messageValidationTest
-
-from EndToEndTests.oldScripts import concurrentTest
-
+from EndToEndTests.oldScripts import fileSystemS3Test
+from EndToEndTests.oldScripts import fileSystemGSTest
+from EndToEndTests.oldScripts import loggingTest
+from EndToEndTests.oldScripts import smilesTest
+from EndToEndTests.oldScripts import configOptionsTest
 
 def runLegacyTest(bc, dask_client, drill, spark):
     targetTestGroups = Settings.data["RunSettings"]["targetTestGroups"]
+
+    dir_data_file = Settings.data["TestSettings"]["dataDirectory"]
+    nRals = Settings.data["RunSettings"]["nRals"]
 
     runAllTests = (
         len(targetTestGroups) == 0
     )  # if targetTestGroups was empty the user wants to run all the tests
 
-    dir_data_file = Settings.data["TestSettings"]["dataDirectory"]
-    nRals = Settings.data["RunSettings"]["nRals"]
+    if runAllTests or ("hiveFileTest" in targetTestGroups):
+        hiveFileTest.main(dask_client, spark, dir_data_file, bc, nRals)
+
+    # OK OK OK - NO SALE
+    if runAllTests or ("unsignedTypeTest" in targetTestGroups):
+        unsignedTypeTest.main(dask_client, drill, dir_data_file, bc, nRals)
 
     if runAllTests or ("columnBasisTest" in targetTestGroups):
         columnBasisTest.main(dask_client, drill, dir_data_file, bc, nRals)
 
-    # if runAllTests or ("hiveFileTest" in targetTestGroups):
-    #     hiveFileTest.main(dask_client, spark, dir_data_file, bc, nRals)
+    if runAllTests or ("dateTest" in targetTestGroups):
+        dateTest.main(dask_client, drill, spark, dir_data_file, bc, nRals)
+
+    if runAllTests or ("booleanTest" in targetTestGroups):
+        booleanTest.main(dask_client, drill, dir_data_file, bc, nRals)
 
     # HDFS is not working yet
     # fileSystemHdfsTest.main(dask_client, drill, dir_data_file, bc)
@@ -35,15 +48,12 @@ def runLegacyTest(bc, dask_client, drill, spark):
     # HDFS is not working yet
     # mixedFileSystemTest.main(dask_client, drill, dir_data_file, bc)
 
+    # FAIL FAIL FAIL
+    # if runAllTests or ("tpchQueriesTest" in targetTestGroups):
+    #     tpchQueriesTest.main(dask_client, drill, spark, dir_data_file, bc, nRals)
+
     if runAllTests or ("messageValidationTest" in targetTestGroups):
         messageValidationTest.main(dask_client, drill, dir_data_file, bc, nRals)
-
-    if runAllTests or ("concurrentTest" in targetTestGroups):
-        concurrentTest.main(dask_client, drill, dir_data_file, bc, nRals)
-
-    if runAllTests or ("configOptionsTest" in targetTestGroups):
-        configOptionsTest.main(dask_client, drill, dir_data_file, bc, nRals)
-
 
     testsWithNulls = Settings.data["RunSettings"]["testsWithNulls"]
     if testsWithNulls != "true":
@@ -53,3 +63,16 @@ def runLegacyTest(bc, dask_client, drill, spark):
 
             if runAllTests or ("fileSystemGSTest" in targetTestGroups):
                 fileSystemGSTest.main(dask_client, drill, dir_data_file, bc, nRals)
+
+    if runAllTests or ("loggingTest" in targetTestGroups):
+        loggingTest.main(dask_client, dir_data_file, bc, nRals)
+
+    # TODO re enable this test once we have the new version of dask
+    # https://github.com/dask/distributed/issues/4645
+    # https://github.com/rapidsai/cudf/issues/7773
+    # if runAllTests or ("smilesTest" in targetTestGroups):
+    #    smilesTest.main(dask_client, spark, dir_data_file, bc, nRals)
+
+    # WARNING!!! This Test must be the last one to test ----------------------------------------------------------------
+    if runAllTests or ("configOptionsTest" in targetTestGroups):
+        configOptionsTest.main(dask_client, drill, spark, dir_data_file, bc, nRals)
