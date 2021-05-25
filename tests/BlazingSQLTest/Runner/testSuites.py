1ae45785e85501dd965fe82294818ea9343005cc
Fernando Rodriguez <diegodfrf@gmail.com>
2021-05-25 10:19:42 -0500

    remove duplicates

--- tests/BlazingSQLTest/Runner/testSuites.py
+++ tests/BlazingSQLTest/Runner/testSuites.py
@@ -45,9 +45,6 @@ class TestSuites():
         self.globalConfig.spark_query = ""
         self.globalConfig.order_by_col = ""
         self.globalConfig.apply_order = True
-        self.globalConfig.use_percentage = False
-        self.globalConfig.acceptable_difference = 0.01
-        self.globalConfig.order_by_col = ""
         self.globalConfig.print_result = True
         self.globalConfig.compare_with = 'drill'
         self.globalConfig.use_percentage = False
