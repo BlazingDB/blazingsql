c6ca10b86fe8545ab3d6268a8abee5aa7d242444
gcca <cristhian@personal.com>
2021-06-18 11:29:54 -0500

    [VDE-287] check get current date replace

--- engine/src/execution_kernels/LogicalFilter.cpp
+++ engine/src/execution_kernels/LogicalFilter.cpp
@@ -5,6 +5,7 @@
 #include "LogicalProject.h"
 #include "parser/expression_utils.hpp"
 #include "utilities/error.hpp"
+#include "Util/StringUtil.h"
 
 namespace ral {
 namespace processor {
@@ -27,10 +28,36 @@ std::unique_ptr<ral::frame::BlazingTable> applyBooleanFilter(
     filteredTable),table.names());
 }
 
+std::string get_current_date_or_timestamp(std::string expression, blazingdb::manager::Context * context) {
+    // We want `CURRENT_TIME` holds the same value as `CURRENT_TIMESTAMP`
+	if (expression.find("CURRENT_TIME") != expression.npos) {
+		expression = StringUtil::replace(expression, "CURRENT_TIME", "CURRENT_TIMESTAMP");
+	}
+
+	std::size_t date_pos = expression.find("CURRENT_DATE");
+	std::size_t timestamp_pos = expression.find("CURRENT_TIMESTAMP");
+
+	if (date_pos == expression.npos && timestamp_pos == expression.npos) {
+		return expression;
+	}
+
+    // CURRENT_TIMESTAMP will return a `ms` format
+	std::string	timestamp_str = context->getCurrentTimestamp().substr(0, 23);
+    std::string str_to_replace = "CURRENT_TIMESTAMP";
+
+	// In case CURRENT_DATE we want only the date value
+	if (date_pos != expression.npos) {
+		str_to_replace = "CURRENT_DATE";
+        timestamp_str = timestamp_str.substr(0, 10);
+	}
+
+	return StringUtil::replace(expression, str_to_replace, timestamp_str);
+}
+
 std::unique_ptr<ral::frame::BlazingTable> process_filter(
   const ral::frame::BlazingTableView & table_view,
   const std::string & query_part,
-  blazingdb::manager::Context * /*context*/) {
+  blazingdb::manager::Context * context) {
 
 	if(table_view.num_rows() == 0) {
 		return std::make_unique<ral::frame::BlazingTable>(cudf::empty_like(table_view.view()), table_view.names());
@@ -40,8 +67,10 @@ std::unique_ptr<ral::frame::BlazingTable> process_filter(
 	if(conditional_expression.empty()) {
 		conditional_expression = get_named_expression(query_part, "filters");
 	}
-
-  std::vector<std::unique_ptr<ral::frame::BlazingColumn>> evaluated_table = evaluate_expressions(table_view.view(), {conditional_expression});
+	
+	conditional_expression = get_current_date_or_timestamp(conditional_expression, context);
+  
+	std::vector<std::unique_ptr<ral::frame::BlazingColumn>> evaluated_table = evaluate_expressions(table_view.view(), {conditional_expression});
 
   RAL_EXPECTS(evaluated_table.size() == 1 && evaluated_table[0]->view().type().id() == cudf::type_id::BOOL8, "Expression does not evaluate to a boolean mask");
 
