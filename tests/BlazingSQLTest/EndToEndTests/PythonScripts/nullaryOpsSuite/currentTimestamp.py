25d05d09becceefe8ca6869905d37487bf0de6c4
gcca <cristhian@personal.com>
2021-07-08 11:01:36 -0500

    [fix/e2e-concatSuite] add current-date test case

--- tests/BlazingSQLTest/EndToEndTests/PythonScripts/nullaryOpsSuite/currentTimestamp.py
+++ tests/BlazingSQLTest/EndToEndTests/PythonScripts/nullaryOpsSuite/currentTimestamp.py
@@ -1,6 +1,11 @@
+from datetime import datetime
+
+import numpy as np
 import cudf
 
+current_date = datetime.now().date()
 
 expected = cudf.DataFrame({
-  'o_orderkey': [2.0, 3.0, 4.0, 5.0, 6.0]
+  'current_date':[np.datetime64(current_date, 'ns')] * 5, 
+  'o_orderkey': [1.0, 2.0, 3.0, 4.0, 5.0]
 }).to_pandas()
